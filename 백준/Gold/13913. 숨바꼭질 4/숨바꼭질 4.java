c67cc37e9a429f06947fb2b7d491b3ef75ea0fa5
uijin-j <80446430+uijin-j@users.noreply.github.com>
2023-11-02 18:09:34 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1456 ms, Memory: 46476 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+  static int[] dp = new int[100001];
+  static int[] parent = new int[100001];
+  static int[] cases = {1, -1, 2};
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+
+    bfs(n, k);
+
+    System.out.println(dp[k] - 1);
+    Stack<Integer> stack = new Stack<>();
+    int index = k;
+    while (index != n) {
+        stack.push(index);
+        index = parent[index];
+    }
+
+    stack.push(index);
+
+    while (!stack.isEmpty()) {
+        System.out.print(stack.pop() + " ");
+    }
+  }
+
+  static void bfs(int start, int goal) {
+    Queue<Integer> q = new LinkedList<>();
+
+    dp[start] = 1;
+    q.offer(start);
+
+    while(!q.isEmpty()) {
+      int now = q.poll();
+
+      if(now == goal) return;
+
+      for(int i = 0; i < 3; ++i) {
+        int next = now + cases[i];
+        if(i == 2) next = now * cases[i];
+
+        if(next >= 0 && next <= 100000 && dp[next] == 0) {
+          dp[next] = dp[now] + 1;
+          parent[next] = now;
+          q.offer(next);
+        }
+      }
+    }
+  }
+}

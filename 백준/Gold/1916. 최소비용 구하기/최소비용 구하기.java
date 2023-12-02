518a3dac78e9896b6644d61cfd64fa4a8dec7b16
uijin-j <80446430+uijin-j@users.noreply.github.com>
2023-12-02 14:43:33 +0900

    [Gold V] Title: 최소비용 구하기, Time: 548 ms, Memory: 50600 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,94 +1,77 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
- 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
- 
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
- 
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
- 
-}
+import java.util.*;
+import java.io.*;
  
 public class Main {
-    static int N, M;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
- 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        Arrays.fill(dist, Integer.MAX_VALUE);
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+    static int n, m;
+    static ArrayList<ArrayList<Bus>> buses;
+    static int[] dist;
+
+    static class Bus implements Comparable<Bus> {
+        int city;
+        int cost;
+    
+        public Bus(int city, int cost) {
+            this.city = city;
+            this.cost = cost;
         }
- 
-        // 단방향 인접 리스트 구현.
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
+    
+        @Override
+        public int compareTo(Bus o) {
+            return cost - o.cost;
         }
- 
-        st = new StringTokenizer(br.readLine());
-        int startPos = Integer.parseInt(st.nextToken());
-        int endPos = Integer.parseInt(st.nextToken());
- 
-        bw.write(dijkstra(startPos, endPos) + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
     }
  
-    // 다익스트라 알고리즘
+    public static void main(String[] args) throws Exception {
+        BufferedReader sb = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(sb.readLine());
+        m = Integer.parseInt(sb.readLine());
+ 
+        buses = new ArrayList<>();
+        dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+ 
+        for (int i = 0; i <= n; i++) {
+            buses.add(new ArrayList<>());
+        }
+ 
+        StringTokenizer st;
+        for (int i = 0; i < m; ++i) {
+            st = new StringTokenizer(sb.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+ 
+            buses.get(from).add(new Bus(to, cost));
+        }
+
+        st = new StringTokenizer(sb.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+ 
+        System.out.println(dijkstra(start, end));
+    }
+ 
     public static int dijkstra(int start, int end) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
+        PriorityQueue<Bus> pq = new PriorityQueue<>();
+        boolean[] check = new boolean[n + 1];
+        pq.offer(new Bus(start, 0));
         dist[start] = 0;
  
         while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
+            Bus now = pq.poll();
  
-            if (!check[cur]) {
-                check[cur] = true;
+            if (!check[now.city]) {
+                check[now.city] = true;
  
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
+                for (Bus next : buses.get(now.city)) {
+                    if (!check[next.city] && dist[next.city] > dist[now.city] + next.cost) {
+                        dist[next.city] = dist[now.city] + next.cost;
+                        pq.add(new Bus(next.city, dist[next.city]));
                     }
                 }
             }
         }
+        
         return dist[end];
     }
 }

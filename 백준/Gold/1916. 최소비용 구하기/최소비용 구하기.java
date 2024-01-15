e42a5d461249c970358bc68202ad59dc2d5fb4e2
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-01-15 22:23:35 +0900

    [Gold V] Title: 최소비용 구하기, Time: 432 ms, Memory: 50508 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,77 +1,64 @@
-import java.util.*;
 import java.io.*;
- 
-public class Main {
-    static int n, m;
-    static ArrayList<ArrayList<Bus>> buses;
-    static int[] dist;
+import java.util.*;
 
-    static class Bus implements Comparable<Bus> {
-        int city;
-        int cost;
-    
-        public Bus(int city, int cost) {
+public class Main
+{
+    static class Node {
+        int city, cost;
+
+        Node(int city, int cost) {
             this.city = city;
             this.cost = cost;
         }
-    
-        @Override
-        public int compareTo(Bus o) {
-            return cost - o.cost;
-        }
     }
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader sb = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(sb.readLine());
-        m = Integer.parseInt(sb.readLine());
- 
-        buses = new ArrayList<>();
-        dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
- 
-        for (int i = 0; i <= n; i++) {
-            buses.add(new ArrayList<>());
-        }
- 
-        StringTokenizer st;
-        for (int i = 0; i < m; ++i) {
-            st = new StringTokenizer(sb.readLine());
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int m = Integer.parseInt(bf.readLine());
+        List<Node>[] buses = new ArrayList[n+1];
+
+        for(int i = 1; i <= n; ++i) buses[i] = new ArrayList<>();
+
+        for(int i = 0; i < m; ++i) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
- 
-            buses.get(from).add(new Bus(to, cost));
+
+            buses[from].add(new Node(to, cost));
         }
 
-        st = new StringTokenizer(sb.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
- 
-        System.out.println(dijkstra(start, end));
-    }
- 
-    public static int dijkstra(int start, int end) {
-        PriorityQueue<Bus> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[n + 1];
-        pq.offer(new Bus(start, 0));
+        int[] dist = new int[n+1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+        pq.offer(new Node(start, 0));
         dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Bus now = pq.poll();
- 
-            if (!check[now.city]) {
-                check[now.city] = true;
- 
-                for (Bus next : buses.get(now.city)) {
-                    if (!check[next.city] && dist[next.city] > dist[now.city] + next.cost) {
-                        dist[next.city] = dist[now.city] + next.cost;
-                        pq.add(new Bus(next.city, dist[next.city]));
-                    }
+        
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.city;
+            int cost = node.cost;
+
+            if(dist[now] < cost) continue;
+            if(now == end) {
+                System.out.println(dist[end]);
+                return;
+            }
+
+            dist[now] = cost;
+
+            for(Node next : buses[now]) {
+                if(dist[next.city] > dist[now] + next.cost) {
+                    dist[next.city] = dist[now] + next.cost;
+                    pq.offer(new Node(next.city, dist[next.city]));
                 }
             }
         }
-        
-        return dist[end];
-    }
+	}
 }

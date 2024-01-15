a3dd92cfa017425b2d779542c4b95613bf880ccf
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-01-16 00:32:07 +0900

    [Gold IV] Title: 서강그라운드, Time: 152 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,63 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static final int INF = 100;
-    static int n, m, r;
-    static int[] items;
-    static int[][] dist;
-
-    public static void main(String[] args) throws Exception {
+public class Main
+{
+    static int INF = 1500;
+	public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
 
-        items = new int[n+1];
-        dist = new int[n+1][n+1];
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                if(i == j) dist[i][j] = 0;
-                else dist[i][j] = INF;
-            }
-        }
-
+        int[] items = new int[n+1];
         st = new StringTokenizer(bf.readLine());
         for(int i = 1; i <= n; ++i) {
             items[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dists = new int[n+1][n+1];
+        for(int i = 1; i <= n; ++i) {
+            Arrays.fill(dists[i], INF);
+            dists[i][i] = 0;
         }
 
         for(int i = 0; i < r; ++i) {
             st = new StringTokenizer(bf.readLine());
             int area1 = Integer.parseInt(st.nextToken());
             int area2 = Integer.parseInt(st.nextToken());
-            int distance = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
 
-            dist[area1][area2] = Math.min(dist[area1][area2], distance);
-            dist[area2][area1] = Math.min(dist[area2][area1], distance);
+            dists[area1][area2] = dist;
+            dists[area2][area1] = dist;
         }
-
+        
         // 플로이드-와샬
         for(int k = 1; k <= n; ++k) {
             for(int i = 1; i <= n; ++i) {
                 for(int j = 1; j <= n; ++j) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    dists[i][j] = Math.min(dists[i][j], dists[i][k] + dists[k][j]);
                 }
             }
         }
 
-        int answer = 0;
-        for(int i = 1; i <= n; ++i) { // i 도시에 떨어졌을 때
+        int max = 0;
+        for(int i = 1; i <= n; ++i) { // i 지역에 떨어졌을 때
             int sum = 0;
             for(int j = 1; j <= n; ++j) {
-                if(dist[i][j] <= m) {
+                if(dists[i][j] <= m) {
                     sum += items[j];
                 }
             }
-            answer = Math.max(answer, sum);
+            max = Math.max(max, sum);
         }
 
-        System.out.println(answer);
-    }
+        System.out.println(max);
+	}
 }

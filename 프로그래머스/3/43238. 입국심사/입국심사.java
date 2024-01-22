11811206b168940154185ae4d03eb958f9618614
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-01-22 16:28:44 +0900

    [level 3] Title: 입국심사, Time: 0.54 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -4,7 +4,7 @@ class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
         long max = (long) times[times.length - 1] * n;
-        long min = -1;
+        long min = 1;
         long answer = max;
         
         while(min <= max) {

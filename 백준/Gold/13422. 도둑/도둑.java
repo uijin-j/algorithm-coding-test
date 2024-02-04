979718fa552ca7c877c360a7741d5163ef5583ae
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-02-04 21:57:37 +0900

    Update 도둑.java

--- 백준/Gold/13422. 도둑/도둑.java
+++ 백준/Gold/13422. 도둑/도둑.java
@@ -3,43 +3,43 @@ import java.util.*;
 
 public class Main
 {
-    static int n, m, k;
-    static int[] money;
+	static int n, m, k;
+	static int[] money;
 
 	public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
-        int t = Integer.parseInt(bf.readLine());
-
-        while(t > 0) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            
-            money = new int[n];
-            st = new StringTokenizer(bf.readLine());
-            for(int i = 0; i < n; ++i) money[i] = Integer.parseInt(st.nextToken());
-
-            int sum = 0, count = 0;
-            for(int i = 0; i < m; ++i) sum += money[i];
-            if(sum < k) ++count;
-
-            if(m == n) {
-                answer.append(count).append("\n");
-                --t;
-                continue;
-            }
-
-            for(int i = m; i < n + m - 1; ++i) {
-                sum += (money[i % n] - money[i - m]);
-                if(sum < k) count++;
-            }
-
-            answer.append(count).append("\n");
-            --t;
-        }
-
-        System.out.print(answer);
-    }
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	StringBuilder answer = new StringBuilder();
+	int t = Integer.parseInt(bf.readLine());
+	
+	while(t > 0) {
+	    StringTokenizer st = new StringTokenizer(bf.readLine());
+	    n = Integer.parseInt(st.nextToken());
+	    m = Integer.parseInt(st.nextToken());
+	    k = Integer.parseInt(st.nextToken());
+	    
+	    money = new int[n];
+	    st = new StringTokenizer(bf.readLine());
+	    for(int i = 0; i < n; ++i) money[i] = Integer.parseInt(st.nextToken());
+	
+	    int sum = 0, count = 0;
+	    for(int i = 0; i < m; ++i) sum += money[i];
+	    if(sum < k) ++count;
+	
+	    if(m == n) {
+		answer.append(count).append("\n");
+		--t;
+		continue;
+	    }
+	
+	    for(int i = m; i < n + m - 1; ++i) {
+		sum += (money[i % n] - money[i - m]);
+		if(sum < k) count++;
+	    }
+	
+	    answer.append(count).append("\n");
+	    --t;
+	}
+	
+	System.out.print(answer);
+	}
 }

1e7ee595e5717bc681c27e135bd12795b1020be7
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-01 17:05:23 +0900

    Update 사다리타기.java

--- 인프런/시뮬레이션 & 구현/사다리타기.java
+++ 인프런/시뮬레이션 & 구현/사다리타기.java
@@ -3,8 +3,8 @@ class Solution {
     // n: 학생 수 (세로 줄의 갯수)
     // ladder[i]: i번째 가로줄과 연결된 위치
     char[] alpha = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	public char[] solution(int n, int[][] ladder){
-		char[] answer = new char[n];
+    public char[] solution(int n, int[][] ladder){
+        char[] answer = new char[n];
         boolean[][] connections = new boolean[ladder.length][n+1];
 
         for(int i = 0; i < ladder.length; ++i) {
@@ -22,14 +22,14 @@ class Solution {
             answer[position - 1] = alpha[i - 1];
         }
 
-		return answer;
-	}
+	return answer;
+    }
 
-	public static void main(String[] args){
-		Solution T = new Solution();
-		System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
-		System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
-		System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
-		System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
-	}
+    public static void main(String[] args){
+	    Solution T = new Solution();
+	    System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+	    System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+	    System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+	    System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+    }
 }

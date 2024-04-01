a14b1acb09237dd81877a4843d5dd600c18f5ba7
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-01 17:35:17 +0900

    Update 청소.java

--- 인프런/시뮬레이션 & 구현/청소.java
+++ 인프런/시뮬레이션 & 구현/청소.java
@@ -35,31 +35,31 @@ class Solution {
 		for(int i = 0; i < k; ++i) {
 		    cleaner.move(board);
 		}
-		
-			return cleaner.getPosition();
-		}
 	
-		public static void main(String[] args){
-			Solution T = new Solution();
-			int[][] arr1 = {{0, 0, 0, 0, 0}, 
-				{0, 1, 1, 0, 0}, 
-				{0, 0, 0, 0, 0}, 
-				{1, 0, 1, 0, 1}, 
-				{0, 0, 0, 0, 0}};
-			System.out.println(Arrays.toString(T.solution(arr1, 10)));
-			int[][] arr2 = {{0, 0, 0, 1, 0, 1}, 
-				{0, 0, 0, 0, 0, 0}, 
-				{0, 0, 0, 0, 0, 1}, 
-				{1, 1, 0, 0, 1, 0}, 
-				{0, 0, 0, 0, 0, 0},
-				{0, 0, 0, 0, 0, 0}};
-			System.out.println(Arrays.toString(T.solution(arr2, 20)));
-			int[][] arr3 = {{0, 0, 1, 0, 0}, 
-				{0, 1, 0, 0, 0}, 
-				{0, 0, 0, 0, 0}, 
-				{1, 0, 0, 0, 1}, 
-				{0, 0, 0, 0, 0}};
-			System.out.println(Arrays.toString(T.solution(arr3, 25)));
-			
-		}
+		return cleaner.getPosition();
+	}
+	
+	public static void main(String[] args){
+		Solution T = new Solution();
+		int[][] arr1 = {{0, 0, 0, 0, 0}, 
+			{0, 1, 1, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr1, 10)));
+		int[][] arr2 = {{0, 0, 0, 1, 0, 1}, 
+			{0, 0, 0, 0, 0, 0}, 
+			{0, 0, 0, 0, 0, 1}, 
+			{1, 1, 0, 0, 1, 0}, 
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr2, 20)));
+		int[][] arr3 = {{0, 0, 1, 0, 0}, 
+			{0, 1, 0, 0, 0}, 
+			{0, 0, 0, 0, 0}, 
+			{1, 0, 0, 0, 1}, 
+			{0, 0, 0, 0, 0}};
+		System.out.println(Arrays.toString(T.solution(arr3, 25)));
+		
+	}
 }

897b12aba0d56e64340c476e412c91df27220a4b
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-01 17:34:07 +0900

    Update 청소.java

--- 인프런/시뮬레이션 & 구현/청소.java
+++ 인프런/시뮬레이션 & 구현/청소.java
@@ -1,44 +1,44 @@
 import java.util.*;
 class Solution {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1}; 
-
-    public static class Cleaner {
-        private int x = 0;
-        private int y = 0;
-        private int direction = 1;
-
-        public void move(int[][] board) {
-            int n = board.length;
-            int nx = x + dx[direction];
-            int ny = y + dy[direction];
-
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 0) {
-                x = nx;
-                y = ny;
-            } else {
-                turn();
-            }
-        }
-
-        public int[] getPosition() {
-            return new int[]{x, y};
-        }
-
-        private void turn() {
-            direction = (direction + 1) % 4;
-        }
-    } 
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, 1, 0, -1}; 
+	
+	public static class Cleaner {
+		private int x = 0;
+		private int y = 0;
+		private int direction = 1;
+	
+		public void move(int[][] board) {
+		    int n = board.length;
+		    int nx = x + dx[direction];
+		    int ny = y + dy[direction];
+		
+		    if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 0) {
+			x = nx;
+			y = ny;
+		    } else {
+			turn();
+		    }
+		}
+		
+		public int[] getPosition() {
+		    return new int[]{x, y};
+		}
+		
+		private void turn() {
+		    direction = (direction + 1) % 4;
+		}
+	} 
 
 	public int[] solution(int[][] board, int k){
-        Cleaner cleaner = new Cleaner();
-        for(int i = 0; i < k; ++i) {
-            cleaner.move(board);
-        }
-
+	Cleaner cleaner = new Cleaner();
+	for(int i = 0; i < k; ++i) {
+	    cleaner.move(board);
+	}
+	
 		return cleaner.getPosition();
 	}
-
+	
 	public static void main(String[] args){
 		Solution T = new Solution();
 		int[][] arr1 = {{0, 0, 0, 0, 0}, 

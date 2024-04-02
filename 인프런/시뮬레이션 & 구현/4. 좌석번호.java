1e95b349a09bfa6809b912cb4e75208e023fc014
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-02 23:53:49 +0900

    Update 4. 좌석번호.java

--- 인프런/시뮬레이션 & 구현/4. 좌석번호.java
+++ 인프런/시뮬레이션 & 구현/4. 좌석번호.java
@@ -4,25 +4,25 @@ class Solution {
     static int[] dy = {1, 0, -1, 0};
 
 	public int[] solution(int c, int r, int k){
-        if(k > r * c) return new int[]{0, 0};
-
-        int x = 1, y = 1;
-        int direction = 0;
-        int[][] seats = new int[c + 1][r + 1];
-        seats[1][1] = 1;
-        for(int i = 2; i <= k; ++i) {
-            int nx = x + dx[direction];
-            int ny = y + dy[direction];
-
-            if(nx >= 1 && nx <= c && ny >= 1 && ny <= r && seats[nx][ny] == 0) {
-                x = nx;
-                y = ny;
-                seats[x][y] = i;
-            } else {
-                direction = (direction + 1) % 4;
-                --i;
-            }
-        }
+	        if(k > r * c) return new int[]{0, 0};
+	
+	        int x = 1, y = 1;
+	        int direction = 0;
+	        int[][] seats = new int[c + 1][r + 1];
+	        seats[1][1] = 1;
+	        for(int i = 2; i <= k; ++i) {
+	            int nx = x + dx[direction];
+	            int ny = y + dy[direction];
+	
+	            if(nx >= 1 && nx <= c && ny >= 1 && ny <= r && seats[nx][ny] == 0) {
+	                x = nx;
+	                y = ny;
+	                seats[x][y] = i;
+	            } else {
+	                direction = (direction + 1) % 4;
+	                --i;
+	            }
+	        }
 		
 		return new int[]{x, y};
 	}

f26cd20381d885ae9105950c677c6b3e1e9d13c5
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-10 21:48:30 +0900

    Update 가장 긴 짝수 연속한 부분 수열 （large）.java

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -10,36 +10,36 @@ public class Main
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		boolean[] isEven = new boolean[n];
+	int n = Integer.parseInt(st.nextToken());
+	int k = Integer.parseInt(st.nextToken());
+	boolean[] isEven = new boolean[n];
 
-		st = new StringTokenizer(bf.readLine());
-		for(int i = 0; i < n; ++i) {
-			int num = Integer.parseInt(st.nextToken());
-			isEven[i] = num % 2 == 0;
-		}
+	st = new StringTokenizer(bf.readLine());
+	for(int i = 0; i < n; ++i) {
+		int num = Integer.parseInt(st.nextToken());
+		isEven[i] = num % 2 == 0;
+	}
 
-		// 풀이봄.. 투포인터! O(n)
-		int left = 0, right = 0, count = 0;
-		int max = 0;
-		while(right < n) {
-			if(!isEven[right]) { // right가 홀수면
-				while(count >= k) {
-					if(!isEven[left]) {
-						--count;
-					}
-
-					++left;
+	// 풀이봄.. 투포인터! O(n)
+	int left = 0, right = 0, count = 0;
+	int max = 0;
+	while(right < n) {
+		if(!isEven[right]) { // right가 홀수면
+			while(count >= k) {
+				if(!isEven[left]) {
+					--count;
 				}
 
-				++count; // 지우기
+				++left;
 			}
 
-			max = Math.max(max, (right - left + 1) - count);
-			++right;
+			++count; // 지우기
 		}
 
-		System.out.println(max);
+		max = Math.max(max, (right - left + 1) - count);
+		++right;
+	}
+
+	System.out.println(max);
     }
 }

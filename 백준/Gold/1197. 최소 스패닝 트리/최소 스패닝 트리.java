7da3a712af43917f22e18e101f5cee2ee8cd854c
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-12 22:56:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 632 ms, Memory: 54656 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,57 +1,58 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-  static List<Edge>[] graph; // graph[i]는 i번째 노드와 연결된 간선들
-  static boolean[] visited;
- 
-  static class Edge {
-    int to;
-    int cost;
-
-    public Edge(int to, int cost) {
-      this.to = to;
-      this.cost = cost;
-    }
-  }
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    int v = sc.nextInt();
-    int e = sc.nextInt();
-    visited = new boolean[v+1];
-    graph = new ArrayList[v + 1];
-    for (int i = 0; i <= v; i++) graph[i] = new ArrayList<>();
-
-    for(int i = 0; i < e; ++i) {
-      int from = sc.nextInt();
-      int to = sc.nextInt();
-      int cost = sc.nextInt();
-
-      graph[from].add(new Edge(to, cost));
-      graph[to].add(new Edge(from, cost));
-    }
-
-    // prim
-    PriorityQueue<Edge> q = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-    q.offer(new Edge(1, 0));
-
-    int answer = 0;
-    while (!q.isEmpty()) {
-      Edge now = q.poll();
-
-      if(visited[now.to]) continue;
-
-      visited[now.to] = true;
-      answer += now.cost;
-
-      for(Edge edge : graph[now.to]) {
-        if(!visited[edge.to]) {
-          q.offer(edge);
+public class Main
+{
+    public static class Edge {
+        int to, cost;
+        
+        public Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
-      }
     }
-
-    System.out.println(answer);
-  }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        List<Edge>[] graph = new List[v+1];
+        for(int i = 1; i <= v; ++i) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < e; ++i) {
+            st = new StringTokenizer(bf.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            
+            graph[v1].add(new Edge(v2, cost));
+            graph[v2].add(new Edge(v1, cost));
+        }
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+        pq.offer(new Edge(1, 0));
+        boolean[] visited = new boolean[v+1];
+        int answer = 0;
+        while(!pq.isEmpty()) {
+            Edge now = pq.poll();
+            
+            if(visited[now.to]) {
+                continue;
+            }
+            
+            visited[now.to] = true;
+            answer += now.cost;
+            
+            for(Edge next : graph[now.to]) {
+                if(visited[next.to]) continue;
+                pq.offer(next);
+            }
+        }
+    	  
+    	  
+        System.out.println(answer);
+    }
 }

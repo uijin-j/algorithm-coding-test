61e0eb12f4098a711e2c289fa6deceb024405b7d
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-19 20:47:11 +0900

    [level 3] Title: 입국심사, Time: 0.37 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,23 +3,24 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-        long max = (long) times[times.length - 1] * n;
-        long min = 1;
-        long answer = max;
         
-        while(min <= max) {
-            long mid = (max + min) / 2;
-            long sum = 0;
+        long left = 1;
+        long right = (long) n * times[times.length - 1];
+        long  answer = right;
+        while(left <= right) {
+            long mid = left + (right - left) / 2;
             
+            long sum = 0;
             for(int time : times) {
                 sum += mid / time;
+                if(sum >= n) break;
             }
             
             if(sum >= n) {
                 answer = mid;
-                max = mid - 1;
+                right = mid - 1;
             } else {
-                min = mid + 1;
+                left = mid + 1;
             }
         }
         

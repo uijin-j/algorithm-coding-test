5c159201b1e899dbd75aaf831ce828432d1742aa
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-04-20 13:40:52 +0900

    [Silver III] Title: 징검다리, Time: 204 ms, Memory: 16892 KB -BaekjoonHub

--- 백준/Silver/11561. 징검다리/징검다리.java
+++ 백준/Silver/11561. 징검다리/징검다리.java
@@ -2,10 +2,6 @@ import java.io.*;
 
 public class Main
 {
-	// 1 2 3 4 ... n
-	// 1번에서 점프
-	// N번은 무조건 밟기
-	// 밟을 수 있는 최대 징검다리
 	// 1부터 k까지 합 = (k + 1) / 2 * k
 	// 즉, k번 밟기 위해서는 최소 (k + 1) / 2 * k 가 n이하여야 함!
     public static void main(String[] args) throws IOException {
@@ -20,12 +16,8 @@ public class Main
 		    long answer = 1;
 
 		    while(left <= right) {
-		        long mid = left + (right - left) / 2; // mid번 밟아서 갈 수 있는가?
-		        long count = (mid + 1) / 2 * mid ;
-				
-		        if(mid % 2 == 0) {
-		            count += mid / 2;
-		        }
+		        long mid = left + (right - left) / 2;
+		        long count = mid * (mid + 1) / 2 ;
 		        
 		        if(count <= n) {
 		            left = mid + 1;

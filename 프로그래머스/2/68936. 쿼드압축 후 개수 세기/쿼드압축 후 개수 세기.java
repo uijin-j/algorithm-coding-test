cc3b80ad7d6c24db0e3f80b620b12a7af30bf6dd
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-06-17 23:03:53 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 26.67 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] solution(int[][] arr) {
+        int[] answer = new int[2];
+        
+        String result = compress(arr, 0, 0, arr.length);
+        for(char ch : result.toCharArray()) {
+            if(ch == '0') answer[0]++;
+            else answer[1]++;
+        }
+        
+        return answer;
+    }
+    
+    public String compress(int[][] arr, int x, int y, int n) {
+        int num = arr[x][y];
+        boolean canCompress = true;
+        for(int i = x; i < x + n; ++i) {
+            for(int j = y; j < y + n; ++j) {
+                if(arr[i][j] != num) {
+                    canCompress = false;
+                    break;
+                }
+            }
+            if(!canCompress) break;
+        }
+        
+        if(canCompress) {
+            return Integer.toString(num);
+        }
+        
+        return compress(arr, x, y, n/2) + compress(arr, x, y + n/2, n/2) + compress(arr, x + n/2, y, n/2) + compress(arr, x + n/2, y + n/2, n/2);
+    }
+}

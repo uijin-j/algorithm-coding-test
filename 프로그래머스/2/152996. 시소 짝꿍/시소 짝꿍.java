9c654b6d70d062e28b5b24090b7ce6042e6de09d
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-06-27 13:55:34 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.09 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/** 
+ * 풀이 1) 모든 사람들을 비교하면서 짝궁인지 확인! O(n^2) => 시간초과!
+ * 풀이 2) 몸무게별로 해당 몸무게를 가진 사람을 count -> 몸무게를 돌며 짝궁 몸무게를 찾아서 경우의 수를 구하기! O(m)
+ * 주의사항) 모든 사람이 같은 몸무게를 가질 경우, 경우의 수를 구할 때 int 범위를 넘길 수도 있음! (long)으로 변환해주기!
+ */
+
 class Solution {
     public long solution(int[] weights) {
         long[] counter = new long[1001];

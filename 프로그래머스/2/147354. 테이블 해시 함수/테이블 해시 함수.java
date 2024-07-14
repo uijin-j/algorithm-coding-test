b7206296d168e5ea776d1d48ac189916a83148c1
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-07-14 21:24:06 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.38 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,19 +1,6 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[][] data, int col, int row_begin, int row_end) { 
-        Arrays.sort(data, (a, b) -> a[col-1] - b[col-1] == 0 ? b[0] - a[0] : a[col-1] - b[col-1]);
-        
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
-        for(int i = row_begin; i <= row_end; ++i) {
-            int sum = 0;
-            for(int column : data[i-1]) {
-                sum += column % i;
-            }
-            
-            answer = answer ^ sum;
-        }
-        
         return answer;
     }
 }

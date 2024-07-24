d43d82d01dcb1ce5b3a5128835f38abd4427af41
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-07-24 22:34:40 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 874.67 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,13 +1,12 @@
 class Solution {
+    // y = (-h / w) * x + h
     public long solution(int w, int h) {
-        long total = w * (long) h;
-        
-        // y = (-h / w)*x + h
+        long total = w * (long) h; // 자료형에 주의하기!
         
         double before = h;
         long count = 0;
         for(long x = 1; x <= w; ++x) {
-            double y = -1 * h * x / (double) w + h;
+            double y = -1 * h * x / (double) w + h; // 정확성을 위해 * 를 먼저 해야함!
             count += before - Math.floor(y);
             before = Math.ceil(y);
         }

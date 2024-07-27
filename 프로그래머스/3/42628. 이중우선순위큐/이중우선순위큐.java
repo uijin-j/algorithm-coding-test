9019fdc65b7297c5be672c5fcb0cfaf2369be4b4
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-07-27 15:13:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.16 ms, Memory: 145 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public class DoublePriorityQueue {
+        private PriorityQueue<Integer> asc;
+        private PriorityQueue<Integer> desc;
+        
+        public DoublePriorityQueue() {
+            this.asc = new PriorityQueue<>((a, b) -> a - b);
+            this.desc = new PriorityQueue<>((a, b) -> b - a);
+        }
+        
+        public void add(Integer e) {
+            asc.add(e);
+            desc.add(e);
+        }
+        
+        public void deleteMax() {
+            if(desc.isEmpty()) return;
+            
+            Integer toDelete = desc.poll();
+            asc.remove(toDelete);
+        }
+        
+        public void deleteMin() { 
+            if(asc.isEmpty()) return;
+            
+            Integer toDelete = asc.poll();
+            desc.remove(toDelete);
+        }
+        
+        public int getMax() {
+            return desc.isEmpty() ? 0 : desc.peek();
+        }
+        
+        public int getMin() {
+            return asc.isEmpty() ? 0 : asc.peek();
+        }
+    }
+    
+    public int[] solution(String[] operations) {
+        DoublePriorityQueue pq = new DoublePriorityQueue();
+        for(String operation : operations) {
+            String command = operation.split(" ")[0];
+            int number = Integer.parseInt(operation.split(" ")[1]);
+            
+            if(command.equals("I")) {
+                pq.add(number);
+                continue;
+            }
+            
+            if(number == 1) {
+                pq.deleteMax();
+                continue;
+            }
+            
+            pq.deleteMin();
+        }
+        
+        return new int[]{ pq.getMax(), pq.getMin() };
+    }
+}

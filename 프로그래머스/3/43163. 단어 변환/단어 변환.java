93eb6a2f81e6779b15c753926e8b861ce59fbc31
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-07-28 16:34:46 +0900

    [level 3] Title: 단어 변환, Time: 0.23 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        Queue<String> q = new LinkedList<>();
+        Set<String> check = new HashSet<>();
+        check.add(begin);
+        q.offer(begin);
+        
+        int answer = 0;
+        while(!q.isEmpty()) {    
+            int size = q.size();
+            for(int i = 0; i < size; ++i) {
+                String word = q.poll();
+                for(int idx = 0; idx < words.length; ++idx) {
+                    String next = words[idx];
+                    if(check.contains(next)) continue;
+                    if(!canConvert(word, next)) continue;
+                    if(next.equals(target)) return answer + 1;
+                    check.add(next);
+                    q.offer(next);
+                }
+            }
+            
+            answer++;
+        }
+        
+        return 0;
+    }
+    
+    public boolean canConvert(String from, String to) {
+        int len = from.length();
+        int count = 0;
+        for(int i = 0; i < len; ++i) {
+            if(from.charAt(i) != to.charAt(i)) count++;
+            if(count > 1) return false;
+        }
+        
+        if(count == 0) return false;
+        
+        return true;
+    }
+}

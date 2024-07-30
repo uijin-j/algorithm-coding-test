cbfd149eae2db0c2e5982959e002303000ac3853
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-07-30 14:23:11 +0900

    [level 3] Title: 등굣길, Time: 0.64 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        // [n, m]까지 가는 방법 => [n-1, m]까지 가는 방법 + [n, m-1]까지 가는 방법
+        boolean[][] blocked = new boolean[n+1][m+1];
+        for(int[] puddle : puddles) {
+            blocked[puddle[1]][puddle[0]] = true;
+        }
+        
+        int[][] dp = new int[n+1][m+1];
+        dp[1][1] = 1;
+        
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j <= m; ++j) {
+                if(i == 1 && j == 1) continue;
+                if(blocked[i][j]) continue;
+                
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
+            }
+        }
+        
+        return dp[n][m]  % 1_000_000_007;
+    }
+}

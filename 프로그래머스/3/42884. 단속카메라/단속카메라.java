3f7be5470200bae206bbbcbd1d49812036a69a8b
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-02 22:18:21 +0900

    [level 3] Title: 단속카메라, Time: 10.37 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 class Solution {
+    // ❗️ 시간/범위가 겹치는 것을 구할 때는 정렬이 좋은 Hint!
     public int solution(int[][] routes) {
         Arrays.sort(routes, (a, b) -> a[0] - b[0] == 0 ? a[1] - b[1] : a[0] - b[0]);
         
         int answer = 0;
-        int before = -30000;
+        int before = -30000; // 이전 cctv 범위가 끝나는 값
         for(int[] route : routes) {
             if(before < route[0]) {
                 answer++;

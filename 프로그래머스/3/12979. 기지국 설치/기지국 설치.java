e0c490b66382a5d3e4361b7053f645b2d06d2f30
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-03 14:37:33 +0900

    [level 3] Title: 기지국 설치, Time: 0.61 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,29 @@
+class Solution {
+    // N이 크기 때문에 O(n) 이하로 해결해야 함!
+    // 그리디?
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int range = 1 + 2 * w;
+        int toCover = 1;
+        for(int station : stations) {
+            int begin = station - w;
+            int end = station + w;
+            
+            // toCover 부터 begin-1까지 커버해야 함!
+            if(toCover < begin) {
+                int gap = begin - toCover;
+                answer += gap / range + (((gap % range) > 0) ? 1 : 0);
+            }
+            
+            toCover = end + 1;
+        }
+        
+        if(toCover <= n) {
+            int gap = n - toCover + 1;
+            int count = gap / range + (((gap % range) > 0) ? 1 : 0);
+            answer += count;
+        }
+
+        return answer;
+    }
+}

cf395e292c3b8c095d030126400c037f73507bdf
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-04 17:08:40 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.39 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,13 +1,13 @@
 class Solution {
     // 항상 가장 큰 숫자를 떼어간다고 최댓값이 아님!
-    // DP! => ❗️ 문제는 순환한다는 것..
+    // DP! => ❗️ 문제는 순환한다는 것.. 첫번째 선택이 마지막 요소에도 영향을 미치기 떄문에, 첫번째 선택O/선택X 경우를 나눠서 생각하자!
     public int solution(int sticker[]) {
         int n = sticker.length;
         
         if(n == 1) return sticker[0];
         
         // 첫번째 스티커를 뗐을 때
-        int[] dp = new int[n];
+        int[] dp = new int[n]; // dp[i]: i번째 스티커까지 고려했을 때, 합의 최대
         dp[0] = sticker[0];
         dp[1] = sticker[0];
         for(int i = 2; i < n-1; ++i) {

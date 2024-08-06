bde81f0850f79fa7a2cd39d09b260b957fff49f8
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-06 22:25:03 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.73 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -13,12 +13,12 @@ class Solution {
         while(right < gems.length) {
             count.put(gems[right], count.getOrDefault(gems[right], 0) + 1);
             
-            while(count.get(gems[left]) > 1) {
+            while(count.get(gems[left]) > 1) { // ❗️ 얘를 while로 해야함! 새로운 left도 확인해야 해서!
                 count.put(gems[left], count.get(gems[left]) - 1);
                 left++;
             }
             
-            if(count.size() == total && length > (right - left + 1)) {
+            if(count.size() == total && length > (right - left + 1)) { // ❗️ 조건에 만족하고, 길이가 더 짧으면 선택!
                 length = right - left + 1;
                 answer = new int[]{left + 1, right + 1};
             }

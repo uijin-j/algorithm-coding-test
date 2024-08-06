884b5ca0cace55b09a5c5ae0f053aa99363ba2b3
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-06 22:05:47 +0900

    [level 3] Title: 불량 사용자, Time: 43.05 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 class Solution {
     // 각 banned_id 요소에 매치되는 아이디들을 탐색
+    // 가능한 조합을 dfs!
+    // 중복을 허용하지 않기 떄문에 추가적인 작업이 필요!
     boolean[] check;
-    int[] selected;
     List<List<Integer>> matchs;
     Set<String> set = new HashSet<>();
     public int solution(String[] user_id, String[] banned_id) {
@@ -12,14 +13,13 @@ class Solution {
             matchs.add(new ArrayList<>());
         }
         
-        for(int idx = 0; idx < user_id.length; ++idx) {
+        for(int idx = 0; idx < user_id.length; ++idx) { // O(n^2) => 64
             for(int i = 0; i < banned_id.length; ++i) {
                 if(match(user_id[idx], banned_id[i])) matchs.get(i).add(idx);
             }
         }
         
         check = new boolean[user_id.length];
-        selected = new int[banned_id.length];
         dfs(0, banned_id.length);
         
         return set.size();
@@ -39,16 +39,19 @@ class Solution {
     
     public void dfs(int level, int goal) {
         if(level == goal) {
-            int[] copy = Arrays.copyOf(selected, selected.length);
-            Arrays.sort(copy);
-            set.add(Arrays.toString(copy));
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < check.length; ++i) {
+                if(check[i]) sb.append(i);
+            }
+            
+            set.add(sb.toString());
             return;
         }
         
         for(int candi : matchs.get(level)) {
             if(check[candi]) continue;
+            
             check[candi] = true;
-            selected[level] = candi;
             dfs(level + 1, goal);
             check[candi] = false;
         }

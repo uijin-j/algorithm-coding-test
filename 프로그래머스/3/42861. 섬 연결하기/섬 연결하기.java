69df6ffa38cb8136e94a8bda60de44dd4280a52d
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-08 22:09:30 +0900

    [level 3] Title: 섬 연결하기, Time: 0.69 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,46 +1,40 @@
 import java.util.*;
 
 class Solution {
-    // 최단거리 문제 -> 다익스트라?
-    // 근데, 지나온 길을 알아야함.. 오토에버 문제랑 비슷한 느낌..
-    public class Node {
-        int to;
-        int cost;        
-        public Node(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
-    }
-    
-    public int solution(int n, int[][] costs) {    
-        List<List<Node>> list = new ArrayList<>();
+    // 최단거리 문제 ... X 최소 스패닝 트리 문제!
+    // MST:  최소 비용으로 모든 노드를 연결하고자 할 때 사용 (크루스칼/프림 알고리즘)
+    // 다익스트라: 단일 출발점에서 다른 모든 정점까지의 최단 경로 찾기
+    int[] parent;
+    public int solution(int n, int[][] costs) {
+        // 크루스칼 알고리즘
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]); // 엣지를 비용을 기준으로 오름차순
+        
+        parent = new int[n];
         for(int i = 0; i < n; ++i) {
-            list.add(new ArrayList<>());
+            parent[i] = i;
         }
         
-        for(int[] cost : costs) {
-            list.get(cost[0]).add(new Node(cost[1], cost[2]));
-            list.get(cost[1]).add(new Node(cost[0], cost[2]));
-        }
-        
-        boolean[] visited = new boolean[n];
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-        pq.add(new Node(0, 0));
         int answer = 0;
-        while(!pq.isEmpty()) {
-            Node node = pq.poll();
-            
-            if(visited[node.to]) continue; // 방문했으면 건너뛰기
-            visited[node.to] = true;
-            answer += node.cost;
-            
-            for(Node next : list.get(node.to)) {
-                if(visited[next.to]) continue;
-                pq.add(new Node(next.to, next.cost));
+        for(int[] cost : costs) { // 가장 작은 비용부터 선택
+            if(find(cost[0]) != find(cost[1])) { // 아직 같은 그래프에 있지 않으면!
+                union(cost[0], cost[1]);
+                answer += cost[2];
             }
         }
         
         return answer;
+    }
+    
+    public int find(int x) {
+        if(parent[x] == x) return x;
+        return find(parent[x]);
+    }
+    
+    public void union(int a, int b) {
+        int parentA = find(a);
+        int patentB = find(b);
+        
+        if(parentA == patentB) return;
+        parent[patentB] = parentA;
     }
 }

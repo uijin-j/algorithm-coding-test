6cbbc515a4a8069a9df1763694c6776d05023812
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-08-09 16:12:55 +0900

    [level 3] Title: 가장 먼 노드, Time: 72.97 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,51 @@
 import java.util.*;
 
 class Solution {
+    // 1번 노드부터 모든 노드까지 최단거리를 구한 뒤, 그 값들 중 가장 긴 거리를 선택?
+    // O(nlogm)
     public class Node {
-        int n, dist;
+        int to;
+        int cost;
         
-        public Node(int n, int dist) {
-            this.n = n;
-            this.dist = dist;
+        public Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
         }
     }
     
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        List<List<Integer>> list = new ArrayList<>(); // list.get(i)는 i와 연결된 노드
-        for(int i = 0; i <= n; ++i) {
-            list.add(new ArrayList<>());
-        }
-        
+        List<List<Node>> list = new ArrayList<>();
+        for(int i = 0; i <= n; ++i) list.add(new ArrayList<>());
         for(int[] e : edge) {
-            list.get(e[0]).add(e[1]);
-            list.get(e[1]).add(e[0]);
+            list.get(e[0]).add(new Node(e[1], 1));
+            list.get(e[1]).add(new Node(e[0], 1));
         }
         
+        boolean[] check = new boolean[n+1];
         int[] dist = new int[n+1];
-        for(int i = 0; i <= n; ++i) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
-        pq.add(new Node(1, 0));
-        int max = 1;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+        pq.offer(new Node(1, 0));
+        int max = 0;
         while(!pq.isEmpty()) {
             Node node = pq.poll();
             
-            if(dist[node.n] != Integer.MAX_VALUE) continue;
-            dist[node.n] = node.dist;
-            max = Math.max(max, dist[node.n]);
+            if(check[node.to]) continue;
+            dist[node.to] = node.cost;
+            check[node.to] = true;
+            max = Math.max(max, dist[node.to]);
             
-            for(int next : list.get(node.n)) {
-                if(dist[node.n] + 1 < dist[next]) {
-                    pq.add(new Node(next, dist[node.n] + 1));
+            for(Node next : list.get(node.to)) {
+                if(!check[next.to] && dist[next.to] > node.cost + next.cost) {
+                    pq.offer(new Node(next.to, node.cost + next.cost));
                 }
             }
         }
         
         int count = 0;
-        for(int i = 1; i <= n; ++i) {
-            if(dist[i] == max) count++;
+        for(int d : dist) {
+            if(d == max) count++;
         }
         
         return count;

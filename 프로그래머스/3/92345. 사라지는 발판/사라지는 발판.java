6129589c35d06e147992cc579f05c778c11c220f
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-10-02 22:41:05 +0900

    [level 3] Title: 사라지는 발판, Time: 20.72 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -12,51 +12,119 @@ class Solution {
      *
      * Q. 항상 이기는 사람을 어떻게 구할 수 있을까?
      */
+    int aMax = 0;
+    int bMax = 0;
     int min = Integer.MAX_VALUE;
+    char winner;
+    int[][] fix = new int[2][2];
     int n, m;
     public int solution(int[][] board, int[] aloc, int[] bloc) {
         n = board.length;
         m = board[0].length;
         
-        return play(board, aloc, bloc);
+        fix[0][0] = aloc[0];
+        fix[0][1] = aloc[1];
+        fix[1][0] = bloc[0];
+        fix[1][1] = bloc[1];
+
+        play('a', board, aloc, bloc, 0); 
+        
+        System.out.println(aMax);
+        System.out.println(bMax);
+        
+        return winner == 'a' ? aMax : bMax;
     }
     
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    public int play(int[][] board, int[] aloc, int[] bloc) {
-        int x = aloc[0];
-        int y = aloc[1];
-
-        if(board[x][y] == 0) return 0;
-        if(canNotMove(aloc, board)) return 0;
+    public char findWinner(int[] aloc, int[] bloc, int[][] board) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{aloc[0], aloc[1], 0});
         
-        board[x][y] = 0;
-
-        int result = 0;
-        for(int d = 0; d < 4; ++d) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
-                int count = 1 + play(board, bloc, new int[]{nx, ny});
-
-                if(result % 2 == 0) { //  짝수면 A 입장에서 지는 것 / 원래 A가 지는 경우만 있었음
-                    if(count % 2 == 0) { // 지는 경우면, 최대한 많이 움직이기!
-                        result = Math.max(result, count);
-                    } else { // 홀수면 A 입장에서 이기는 것 / 이길 수 있으면 이겨야 함!
-                        result = count;
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            
+            for(int d = 0; d < 4; ++d) {
+                int nx = node[0] + dx[d];
+                int ny = node[1] + dy[d];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
+                    if(nx == bloc[0] && ny == bloc[1]) {
+                        int min = node[2] + 1;
+                        if(min % 2 == 0) return 'a';
+                        return 'b';
                     }
-                } else { // 원래 A가 이기는 경우만 있었음
-                    if(count % 2 == 1) { // 이기는 경우면, 최대한 조금 움직이기!
-                        result = Math.min(result, count);
-                    }
+                    q.offer(new int[]{nx, ny, node[2] + 1});
                 }
             }
         }
         
-        board[x][y] = 1;
+        return 'a';
+    }
+    
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    public void play(char turn, int[][] board, int[] aloc, int[] bloc, int count) {
+        System.out.println("[" + count + "] " + (char) turn + " turn!");
+        System.out.println("a: [" + aloc[0] + ", " + aloc[1] + "]");
+        System.out.println("b: [" + bloc[0] + ", " + bloc[1] + "]");
         
-        return result;
+        if(turn == 'a') {
+            if(canNotMove(aloc, board)) {
+                bMax = Math.max(bMax, count);
+                if(min > count) {
+                    min = count;
+                    winner = 'b';
+                }
+                return;
+            }
+            
+            if(aloc[0] == bloc[0] && aloc[1] == bloc[1]) {
+                aMax = Math.max(aMax, count + 1);
+                if(min > count) {
+                    min = count;
+                    winner = 'a';
+                }
+                return;
+            }
+            
+            changeToNotAvaliable(board, aloc[0], aloc[1]);
+            for(int d = 0; d < 4; ++d) {
+                int nx = aloc[0] + dx[d];
+                int ny = aloc[1] + dy[d];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
+                    play('b', board, new int[]{nx, ny}, bloc, count + 1);
+                }
+            }
+        } else {
+            if(canNotMove(bloc, board)) {
+                aMax = Math.max(aMax, count);
+                if(min > count) {
+                    min = count;
+                    winner = 'a';
+                }
+                return;
+            }
+            
+            if(aloc[0] == bloc[0] && aloc[1] == bloc[1]) {
+                bMax = Math.max(bMax, count + 1);
+                if(min > count) {
+                    min = count;
+                    winner = 'b';
+                }
+                return;
+            }
+            
+            changeToNotAvaliable(board, bloc[0], bloc[1]);
+            
+            for(int d = 0; d < 4; ++d) {
+                int nx = bloc[0] + dx[d];
+                int ny = bloc[1] + dy[d];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
+                    play('a', board, aloc, new int[]{nx, ny}, count + 1);
+                }
+            }
+        }
     }
     
     public boolean canNotMove(int[] loc, int[][] board) {
@@ -70,5 +138,9 @@ class Solution {
         }
         
         return true;
+    }
+    
+    public void changeToNotAvaliable(int[][] board, int x, int y) {        
+        board[x][y] = 0;
     }
 }

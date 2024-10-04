0a6150569475b864c4ee15434093658c19da26f1
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-10-04 23:09:45 +0900

    [level 4] Title: 행렬과 연산, Time: 75.68 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/4/118670. 행렬과 연산/행렬과 연산.java
+++ 프로그래머스/4/118670. 행렬과 연산/행렬과 연산.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 // 16:05 START 17:30 
-// 
 class Solution {
     /**
-     * 무식한 방법 O(n^2c) ~= 10_000_000_000 시간 초과
+     * 무식한 방법 O(nmc) ~= 10_000_000_000 시간 초과
      *
      * 시간을 줄일 수 있는 방법? 연산을 할 때 모든 칸을 돌지 않으면 됨!
      * ShiftRow의 경우에는 직접 원소를 옮기는 것이 아니라, 어떤 열의 순서만 알면됨! (큐 사용하기)
@@ -18,8 +17,6 @@ class Solution {
     public int[][] solution(int[][] rc, String[] operations) {
         int n = rc.length;
         int m = rc[0].length;
-        
-        System.out.println(n + " " + m);
 
         Deque<Integer> firstCol = new ArrayDeque<>();
         Deque<Integer> lastCol = new ArrayDeque<>();

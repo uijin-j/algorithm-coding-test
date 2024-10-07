4c74eb367d6d8cff9640cd5ffde8c545575e09ab
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-10-07 23:07:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 105.75 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -10,9 +10,9 @@ import java.util.*;
  * S노드에서 1..N 노드까지 함께 택시를 타고, 하차 노드에서 A, B 노드까지 최소 비용을 구해서 더하기?
  */ 
 class Solution {
-    static final int INF = 2_000_000_000;
+    static final int INF = 20_000_000;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        long[][] dist = new long[n+1][n+1]; // dist[i][j]는 i노드에서 j노드까지의 최단거리!
+        int[][] dist = new int[n+1][n+1]; // dist[i][j]는 i노드에서 j노드까지의 최단거리!
         
         for(int i = 0; i <= n; ++i) {
             Arrays.fill(dist[i], INF);
@@ -24,17 +24,7 @@ class Solution {
             dist[fare[1]][fare[0]] = fare[2];
         }
         
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                if(i == j) continue;
-                for(int k = 1; k <= n; ++k) {
-                    if(i == k || j == k) continue;
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]); 
-                }
-            }
-        }
-        
-        for(int k = 1; k <= n; ++k) {
+        for(int k = 1; k <= n; ++k) { // ❗️ 플로이드 와샬은 경유지 K가 젤 바깥 for문에 위치한다..
             for(int i = 1; i <= n; ++i) {
                 for(int j = 1; j <= n; ++j) {
                     if(i == j || i == k || j == k) continue;
@@ -45,7 +35,7 @@ class Solution {
         
         int min = Integer.MAX_VALUE;
         for(int i = 1; i <= n; ++i) {
-            min = Math.min(min, (int) (dist[s][i] + dist[i][a] + dist[i][b]));
+            min = Math.min(min, dist[s][i] + dist[i][a] + dist[i][b]);
         }
         
         return min;

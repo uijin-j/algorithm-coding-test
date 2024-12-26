b21a0978e107644989bedfc4556dd5f7146b830d
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-12-26 21:35:40 +0900

    [Gold III] Title: 중량제한, Time: 472 ms, Memory: 54268 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,71 +1,85 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-	static int ans;
-	static List<City>[] list;
-	static boolean[] check;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		list = new ArrayList[n+1];
-		for(int i=1; i<n+1; i++) {
-			list[i] = new ArrayList<>();
-		}
-		int left =0;
-		int right =0;
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			list[a].add(new City(b,w));
-			list[b].add(new City(a,w));
-			right = Math.max(right, w); // 그래프 최대 중량 구하기 
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int from = Integer.parseInt(st.nextToken());
-		int to = Integer.parseInt(st.nextToken());
-		
-		while(left<= right) {
-			int mid = (left+right)/2;
-			ans = -1;
-			check = new boolean[n+1];
-			dfs(from,to,mid);
-			if(ans != -1) { // 이동이 가능하면 중량 올리기
-				left = mid+1;
-			}else { // 이동 불가능하면 중량 줄이기 
-				right = mid-1;
-			}
-		}
-		System.out.println(right);
+// 19:59 시작!
+public class Main
+{
+    /**
+     * A에서 B로 가는 경로 중 다리의 최소 중량이 최대가 되는 경우?
+     * 
+     * sol 1. DFS로 모든 경로를 탐색? O(NM) ~= 1_000_000_000 애매하다..
+	 * sol 2. 다익스트라? O(mlogn) 시간 초과ㅠㅜ
+	 * sol 3. 이분탐색 + DFS
+     */
+    static boolean possible;
+    static List<Node>[] bridges;
+    static boolean[] visit;
+	public static void main(String[] args) throws Exception {
+	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(bf.readLine());
+	    int n = Integer.parseInt(st.nextToken());
+	    int m = Integer.parseInt(st.nextToken());
+	    bridges = new ArrayList[n + 1];
+	    for(int i = 1; i <= n; ++i) {
+	        bridges[i] = new ArrayList<>();
+	    }
+	    
+	    // 1. 입력 받기
+	    int left = 1;
+	    int right = 1;
+	    for(int i = 0; i < m; ++i) {
+	        st = new StringTokenizer(bf.readLine());
+	        int p1 = Integer.parseInt(st.nextToken());
+	        int p2 = Integer.parseInt(st.nextToken());
+	        int limit = Integer.parseInt(st.nextToken());
+	        
+	        bridges[p1].add(new Node(p2, limit));
+	        bridges[p2].add(new Node(p1, limit));
+	        right = Math.max(right, limit);
+	    }
+	    
+	    st = new StringTokenizer(bf.readLine());
+	    int factory1 = Integer.parseInt(st.nextToken());
+	    int factory2 = Integer.parseInt(st.nextToken());
+	    int max = left;
+	    while(left <= right) {
+	        int mid = left + (right - left) / 2;
+	        possible = false;
+	        visit = new boolean[n + 1];
+	        visit[factory1] = true;
+	        dfs(factory1, factory2, mid);
+	        
+	        if(possible) {
+	            max = mid;
+	            left = mid + 1;
+	        } else {
+	            right = mid - 1;
+	        }
+	    }
+	    
+	    System.out.println(max);
 	}
 	
-	static void dfs(int pos, int target, int limit) {
-		if(pos == target) {
-			ans = pos; 
-			return;
-		}
-		check[pos]= true;
-		for(City c : list[pos]) {
-			if(!check[c.to] && limit <= c.w ) {
-				dfs(c.to, target, limit);
-			}
-		}
+	public static void dfs(int island, int goal, int limit) {
+	    if(island == goal) {
+	        possible = true;
+	        return;
+	    }
+	    
+	    for(Node next : bridges[island]) {
+	        if(next.limit >= limit && !visit[next.to]) {
+	            visit[next.to] = true;
+	            dfs(next.to, goal, limit);
+	        }
+	    }
 	}
-}
-class City{
-	int to;
-	int w;
 	
-	public City(int to, int w) {
-		this.to = to;
-		this.w = w;
+	public static class Node {
+	    int to, limit;
+	    
+	    public Node(int to, int limit) {
+	        this.to = to;
+	        this.limit = limit;
+	    }
 	}
 }

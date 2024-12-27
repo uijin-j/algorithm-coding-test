b33faf919147269b13bd39a4ac5e413231a8bca4
uijin-j <80446430+uijin-j@users.noreply.github.com>
2024-12-27 09:45:10 +0900

    [Gold III] Title: 중량제한, Time: 460 ms, Memory: 54540 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -6,27 +6,20 @@ public class Main
 {
     /**
      * A에서 B로 가는 경로 중 다리의 최소 중량이 최대가 되는 경우?
-     * 
-     * sol 1. DFS로 모든 경로를 탐색? O(NM) ~= 1_000_000_000 애매하다..
-	 * sol 2. 다익스트라? O(mlogn) 시간 초과ㅠㅜ
-	 * sol 3. 이분탐색 + DFS
      */
-    static boolean possible;
     static List<Node>[] bridges;
-    static boolean[] visit;
+    static int n, m, factory1, factory2;
 	public static void main(String[] args) throws Exception {
 	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st = new StringTokenizer(bf.readLine());
-	    int n = Integer.parseInt(st.nextToken());
-	    int m = Integer.parseInt(st.nextToken());
-	    bridges = new ArrayList[n + 1];
+	    n = Integer.parseInt(st.nextToken());
+	    m = Integer.parseInt(st.nextToken());
+        bridges = new ArrayList[n + 1];
 	    for(int i = 1; i <= n; ++i) {
 	        bridges[i] = new ArrayList<>();
 	    }
 	    
 	    // 1. 입력 받기
-	    int left = 1;
-	    int right = 1;
 	    for(int i = 0; i < m; ++i) {
 	        st = new StringTokenizer(bf.readLine());
 	        int p1 = Integer.parseInt(st.nextToken());
@@ -35,43 +28,36 @@ public class Main
 	        
 	        bridges[p1].add(new Node(p2, limit));
 	        bridges[p2].add(new Node(p1, limit));
-	        right = Math.max(right, limit);
 	    }
 	    
 	    st = new StringTokenizer(bf.readLine());
-	    int factory1 = Integer.parseInt(st.nextToken());
-	    int factory2 = Integer.parseInt(st.nextToken());
-	    int max = left;
-	    while(left <= right) {
-	        int mid = left + (right - left) / 2;
-	        possible = false;
-	        visit = new boolean[n + 1];
-	        visit[factory1] = true;
-	        dfs(factory1, factory2, mid);
+	    factory1 = Integer.parseInt(st.nextToken());
+	    factory2 = Integer.parseInt(st.nextToken());
+	    
+	    // sol1. DFS로 모든 경로를 탐색? O(NM) ~= 1_000_000_000 애매하다..
+	    // sol2. 다익스트라?
+	    int[] maxLimit = new int[n + 1]; // maxLimit[i]: factory1에서 i까지 경로 중 가장 중량 제한이 큰 경우의 중량 제한
+	    boolean[] check = new boolean[n + 1]; // check[i]: maxLimit[i]가 확정되었는지 확인
+	    
+	    PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> b.limit - a.limit);
+	    pq.offer(new Node(factory1, Integer.MAX_VALUE));
+	    
+	    while(!pq.isEmpty()) {
+	        Node node = pq.poll();
 	        
-	        if(possible) {
-	            max = mid;
-	            left = mid + 1;
-	        } else {
-	            right = mid - 1;
+	        if(check[node.to]) continue;
+	        maxLimit[node.to] = node.limit;
+	        check[node.to] = true;
+
+	        for(Node next : bridges[node.to]) {	            
+	            if(maxLimit[next.to] < Math.min(node.limit, next.limit)) {
+	                maxLimit[next.to] = Math.min(node.limit, next.limit);
+	                pq.offer(new Node(next.to, Math.min(node.limit, next.limit)));
+	            }
 	        }
 	    }
 	    
-	    System.out.println(max);
-	}
-	
-	public static void dfs(int island, int goal, int limit) {
-	    if(island == goal) {
-	        possible = true;
-	        return;
-	    }
-	    
-	    for(Node next : bridges[island]) {
-	        if(next.limit >= limit && !visit[next.to]) {
-	            visit[next.to] = true;
-	            dfs(next.to, goal, limit);
-	        }
-	    }
+	    System.out.println(maxLimit[factory2]);
 	}
 	
 	public static class Node {

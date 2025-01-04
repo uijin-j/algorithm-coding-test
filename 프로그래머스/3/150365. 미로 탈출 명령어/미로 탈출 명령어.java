352c453d68a49d3467fd062c4e32081bab1ce5c5
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-01-04 20:16:22 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 4.41 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,48 +1,49 @@
 import java.util.*;
 
-// 17:06 START!
 class Solution {
     /**
-     * 그래프 탐색 (bfs)
-     * - 일반적인 bfs와 다른 점: 또 방문할 수 있음 / 최단거리가 아닌 일정 거리 k로 가는 방법을 구해야 함
-     * 깊이가 k로 정해져 있으니까 dfs가 더 나을 것 같다! => 중복 방문이 허용되기 때문에 O(4^k)..
+     * (x, y) -> (r, c)로 가는데 중복 방문 ⭕️, 거리 고정 ⭕️
+     * 최단거리를 구하는 dfs 방식으로는 시간초과 O(4^k)
+     * => 계산을 줄일 수 있는 방법을 찾아보기!
      */
-    String answer;
-    boolean found;
+    int n, m;
     int[] target;
-    int n, m, k, r, c;
+    boolean found;
+    String answer = "impossible";
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        answer = "";
-        target = new int[]{r-1, c-1};
         this.n = n;
         this.m = m;
-        this.k = k;
+        target = new int[]{ r - 1, c - 1 };
         
-        int minDistance = Math.abs(x - r) + Math.abs(y - c);
-        if(k < minDistance) return "impossible";
-        if((k - minDistance) % 2 == 1) return "impossible";
+        int min = Math.abs(x - r) + Math.abs(y - c); // 최단거리
+        if( k < min || (k - min) % 2 == 1) {
+            return "impossible"; // 도달할 수 없는 경우
+        }
         
-        StringBuilder sb = new StringBuilder();
-        dfs(0, x-1, y-1, new StringBuilder());
+        dfs(0, x - 1, y - 1, k, new StringBuilder());
         
-        return answer == "" ? "impossible" : answer;
+        return answer;
     }
     
-    String[] d = {"d", "l", "r", "u"};
-    int[] dx = {1, 0, 0, -1};
-    int[] dy = {0, -1, 1, 0};
-    public void dfs(int level, int x, int y, StringBuilder sb) {
-        if(found) return;
-        if(level == k) {
-            if(x == target[0] && y == target[1]) {
-                answer = sb.toString();
-                found = true;
-            }
+    String[] d = { "d", "l", "r", "u" };
+    int[] dx = { 1, 0, 0, -1 };
+    int[] dy = { 0, -1, 1, 0 };
+    public void dfs(int level, int x, int y, int goal, StringBuilder sb) {
+        if(found) {
             return;
         }
         
-        int minDistance = Math.abs(x - target[0]) + Math.abs(y - target[1]);
-        if(k - level < minDistance) return;
+        if(level == goal) {
+            if(x == target[0] && y == target[1]) {
+                found = true;
+                answer = sb.toString();
+            }
+            
+            return;
+        }
+        
+        int min = Math.abs(x - target[0]) + Math.abs(y - target[1]);
+        if(goal - level < min || (goal - level - min) % 2 == 1) return; // 도달할 수 없는 경우
         
         for(int i = 0; i < 4; ++i) {
             int nx = x + dx[i];
@@ -50,8 +51,8 @@ class Solution {
             
             if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
                 sb.append(d[i]);
-                dfs(level + 1, nx, ny, sb);
-                sb.deleteCharAt(sb.length() - 1);
+                dfs(level + 1, nx, ny, goal, sb);
+                sb.deleteCharAt(level);
             }
         }
     }

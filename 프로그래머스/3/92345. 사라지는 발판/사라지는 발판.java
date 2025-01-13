f2f20bdd1e827dae91b7ace9510cc703baabf518
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-01-13 23:45:41 +0900

    [level 3] Title: 사라지는 발판, Time: 19.85 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,73 +1,66 @@
-import java.util.*;
-
-// 10:45 START!
 class Solution {
     /**
-     * 완탐? 
-     * 한번 이동 할 때마다, board의 상태가 변경됨
-     *
-     * 종료 케이스
-     * - A와 B가 같은 위치에 있으면, 먼저 움직이는 사람이 승리!
-     * - 이동할 곳이 없으면 패배
-     *
-     * Q. 항상 이기는 사람을 어떻게 구할 수 있을까?
+     * 두 플레이어가 최선의 게임을 했을 때, 총 이동 횟수
      */
     int n, m;
+    int INF = 25;
     public int solution(int[][] board, int[] aloc, int[] bloc) {
         n = board.length;
         m = board[0].length;
         
-        return play(board, aloc, bloc);
+        return play(board, aloc, bloc).move;
     }
     
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    public int play(int[][] board, int[] player, int[] waiting) {
+    int[] dx = { -1, 0, 1, 0 };
+    int[] dy = { 0, 1, 0, -1 };
+    public Result play(int[][] board, int[] player, int[] waiter) {
         int x = player[0];
         int y = player[1];
-
-        if(board[x][y] == 0) return 0;
-        if(canNotMove(player, board)) return 0;
         
-        board[x][y] = 0;
-
-        int best = 0;
+        if(board[x][y] == 0) return new Result(false, 0);
+        
+        int win = INF;
+        int lose = -INF;
         for(int d = 0; d < 4; ++d) {
             int nx = x + dx[d];
             int ny = y + dy[d];
-
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 1) {
-                int count = 1 + play(board, waiting, new int[]{nx, ny});
-
-                if(best % 2 == 0) { // 짝수면 A 입장에서 지는 것 / 원래 A가 지는 경우만 있었음
-                    if(count % 2 == 0) { // 지는 경우면, 최대한 많이 움직이기!
-                        best = Math.max(best, count);
-                    } else { // 홀수면 A 입장에서 이기는 것 / 이길 수 있으면 이겨야 함!
-                        best = count;
-                    }
-                } else { // 원래 A가 이기는 경우만 있었음
-                    if(count % 2 == 1) { // 이기는 경우면, 최대한 조금 움직이기!
-                        best = Math.min(best, count);
-                    }
+            
+            if(checkBoundary(nx, ny) && board[nx][ny] == 1) { // 갈 수 있다면
+                board[x][y] = 0;
+                Result result = play(board, waiter, new int[]{nx, ny});
+                
+                if(result.win) { // player가 lose
+                    lose = Math.max(lose, 1 + result.move);
+                } else { // player가 win
+                    win = Math.min(win, 1 + result.move);
                 }
+                
+                board[x][y] = 1;
             }
         }
         
-        board[x][y] = 1;
+        if(win == INF && lose == -INF) {
+            return new Result(false, 0);
+        }
         
-        return best;
+        if(win != INF) { // 이길 수 있으면 무조건 이기기
+            return new Result(true, win);
+        }
+        
+        return new Result(false, lose);
     }
     
-    public boolean canNotMove(int[] loc, int[][] board) {
-        for(int d = 0; d < 4; ++d) {
-            int nx = loc[0] + dx[d];
-            int ny = loc[1] + dy[d];
-
-            if(nx >= 0 && nx < board.length && ny >= 0 && ny < board[0].length && board[nx][ny] == 1) {
-                return false;
-            }
-        }
+    public boolean checkBoundary(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+    
+    public class Result {
+        boolean win;
+        int move;
         
-        return true;
+        public Result(boolean win, int move) {
+            this.win = win;
+            this.move = move;
+        }
     }
 }

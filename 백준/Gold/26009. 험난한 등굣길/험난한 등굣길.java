ecbae7f63e7199e714446f97a7a14565aff97834
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-01-19 22:08:27 +0900

    [Gold II] Title: 험난한 등굣길, Time: 804 ms, Memory: 334940 KB -BaekjoonHub

--- 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.java
+++ 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.java
@@ -1,105 +1,106 @@
 import java.io.*;
 import java.util.*;
 
-// 17:10 시작!
-public class Main
-{
+public class Main {
     /**
-     * 모든 k에 대해 정체 되는 칸을 구하고, bfs?
+     * 최단거리를 구하는 문제이기 때문에 BFS or DFS
      */
-    static boolean[][] map;
-    static int[][] check; 
-    static int n, m;
+    static int n, m, k;
+    static int[][] map;
+    static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
 	public static void main(String[] args) throws Exception {
 	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st = new StringTokenizer(bf.readLine());
 	    n = Integer.parseInt(st.nextToken());
 	    m = Integer.parseInt(st.nextToken());
-	    map = new boolean[n+1][m+1];
-	    check = new int[n+1][m+1];
-	    for(int i = 0; i <= n; ++i) {
-	        Arrays.fill(check[i], -1);
-	    }
+	    map = new int[n+1][m+1];
 	    
-	    int k = Integer.parseInt(bf.readLine());
-	    int[][] regions = new int[k][3];
+	    k = Integer.parseInt(bf.readLine());
+	    int[][] jam = new int[k][3];
 	    for(int i = 0; i < k; ++i) {
 	        st = new StringTokenizer(bf.readLine());
-	        int x = Integer.parseInt(st.nextToken());
-	        int y = Integer.parseInt(st.nextToken());
-	        int dist = Integer.parseInt(st.nextToken());
+	        int r = Integer.parseInt(st.nextToken());
+	        int c = Integer.parseInt(st.nextToken());
+	        int d = Integer.parseInt(st.nextToken());
 	        
-	        regions[i] = new int[]{x, y, dist};
+	        fill(r, c, d);
 	    }
 	    
-	    Arrays.sort(regions, (a, b) -> b[2] - a[2]);
-	    
-	     for(int i = 0; i < k; ++i) {
-	        mark(regions[i][0], regions[i][1], regions[i][2]);   
-	     }
-	    
 	    Queue<int[]> q = new LinkedList<>();
-	    q.offer(new int[]{1, 1});
-	    boolean possible = false;
-	    int level = 0;
+	    map[1][1] = 1;
+	    q.offer(new int[]{ 1, 1 });
 	    while(!q.isEmpty()) {
-	        int size = q.size();
-	        for(int i = 0; i < size; ++i) {
-	            int[] p = q.poll();
+	        int[] node = q.poll();
+	        int x = node[0];
+	        int y = node[1];
+
+	        for(int d = 0; d < 4; ++d) {
+	            int nx = x + dx[d];
+	            int ny = y + dy[d];
 	            
-	            for(int d = 0; d < 4; ++d) {
-	                int nx = p[0] + dx[d];
-	                int ny = p[1] + dy[d];
-	                if(nx >= 1 && nx <= n && ny >= 1 && ny <= m && !map[nx][ny]) {
-	                    if(nx == n && ny == m) {
-	                        System.out.println("YES");
-	                        System.out.println(level + 1);
-	                        return;
-	                    }
-	                    
-	                    q.offer(new int[]{nx, ny});
-	                    map[nx][ny] = true;
+	            if(nx > 0 && nx <= n && ny > 0 && ny <= m && map[nx][ny] == 0) {
+	                if(nx == n && ny == m) {
+	                    System.out.println("YES");
+	                    System.out.println(map[x][y]);
+	                    return;
 	                }
+	                
+	                map[nx][ny] = map[x][y] + 1;
+	                q.offer(new int[]{ nx, ny });
 	            }
 	        }
-	        
-	        level++;
 	    }
 	    
 	    System.out.println("NO");
 	}
 	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	public static void mark(int x, int y, int dist) {
-	    if(check[x][y] > dist) return; // 이미 마킹 했으면
+	public static void fill(int x, int y, int d) {
+	    // x - d, y
+	    // x, y + d
+	    // x + d, y
+	    // x, y - d
+	    if(d == 0) {
+	        map[x][y] = 1;
+	        return;
+	    }
 	    
-	    Queue<int[]> q = new LinkedList<>();
-	    q.offer(new int[]{x, y});
-	    map[x][y] = true;
-	    check[x][y] = dist;
-	    
-	    int level = 0;
-	    while(!q.isEmpty()) {
-	        if(level == dist) break;
-	        
-	        int size = q.size();
-	        for(int i = 0; i < size; ++i) {
-	            int[] p = q.poll();
-	            
-	            for(int d = 0; d < 4; ++d) {
-	                int nx = p[0] + dx[d];
-	                int ny = p[1] + dy[d];
-	                if(nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
-	                    if(check[nx][ny] >= dist - (level + 1)) continue;
-	                    check[nx][ny] = dist - (level + 1);
-	                    map[nx][ny] = true;
-	                    q.offer(new int[]{nx, ny});
-	                }
-	            }
+	    int nx = x - d;
+	    int ny = y;
+	    while(nx < x) {
+	        if(nx > 0 && nx <= n && ny > 0 && ny <= m) {
+	            map[nx][ny] = -1;
 	        }
 	        
-	        ++level;
+	        nx++;
+	        ny++;
+	    }
+	    
+	    while(nx < x + d) {
+	        if(nx > 0 && nx <= n && ny > 0 && ny <= m) {
+	            map[nx][ny] = -1;
+	        }
+	        
+	        nx++;
+	        ny--;
+	    }
+	    
+	    while(nx > x) {
+	        if(nx > 0 && nx <= n && ny > 0 && ny <= m) {
+	            map[nx][ny] = -1;
+	        }
+	        
+	        nx--;
+	        ny--;
+	    }
+	    
+	    while(nx > x - d) {
+	        if(nx > 0 && nx <= n && ny > 0 && ny <= m) {
+	            map[nx][ny] = -1;
+	        }
+	        
+	        nx--;
+	        ny++;
 	    }
 	}
 }

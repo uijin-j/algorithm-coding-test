af8c223f8067ac71b8ea5c9ffea776a5ff744bcc
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-02-24 15:01:25 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 52.00 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,44 +1,44 @@
-// 19:49 START!
 class Solution {
-    // 무식한 방법 O(nmk) -> 1000 * 1000 * 250,000 시간초과
-    // 어렵다.. 누적합
     public int solution(int[][] board, int[][] skill) {
         int n = board.length;
         int m = board[0].length;
-        int[][] affected = new int[n+1][m+1];
+        int[][] sum = new int[n+1][m+1];
         
-        for(int[] s : skill) {
-            int toSum = ((s[0] == 1) ? -1 : 1) * s[5];
-            int x1 = s[1];
-            int y1 = s[2];
-            int x2 = s[3];
-            int y2 = s[4];
+        for(int[] info : skill) {
+            int type = info[0];
+            int r1 = info[1];
+            int c1 = info[2];
+            int r2 = info[3];
+            int c2 = info[4];
+            int degree = info[5];
             
-            affected[x1][y1] += toSum;
-            affected[x1][y2+1] += (toSum * -1);
-            affected[x2+1][y1] += (toSum * -1);
-            affected[x2+1][y2+1] += toSum;
+            if(type == 1) degree *= -1;
+            
+            sum[r1][c1] += degree;
+            sum[r1][c2+1] += degree * -1;
+            sum[r2+1][c1] += degree* -1;
+            sum[r2+1][c2+1] += degree;
         }
         
         for(int i = 0; i < n; ++i) {
             for(int j = 1; j < m; ++j) {
-                affected[i][j] += affected[i][j-1];
+                sum[i][j] += sum[i][j-1];
             }
         }
         
         for(int i = 1; i < n; ++i) {
             for(int j = 0; j < m; ++j) {
-                affected[i][j] += affected[i-1][j];
+                sum[i][j] += sum[i-1][j];
             }
         }
         
-        int count = 0;
-         for(int i = 0; i < n; ++i) {
+        int answer = 0;
+        for(int i = 0; i < n; ++i) {
             for(int j = 0; j < m; ++j) {
-                if(board[i][j] + affected[i][j] > 0) count++;
+                if(board[i][j] + sum[i][j] > 0) answer++;
             }
         }
         
-        return count;
+        return answer;
     }
 }

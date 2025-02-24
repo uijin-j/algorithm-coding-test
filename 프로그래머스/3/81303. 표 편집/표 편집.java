8cfdc0d1d8237960ae762ae63a8d6116add9db58
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-02-24 21:37:54 +0900

    [level 3] Title: 표 편집, Time: 347.05 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,61 +1,70 @@
 import java.util.*;
 
-// 22:20 START!
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        int cursor = k;
-        Stack<Integer> history = new Stack<>();
-        int[] prev = new int[n];
-        int[] next = new int[n];
-        
-        for(int i = 0; i < n; ++i) {
-            prev[i] = i - 1;
-            next[i] = i + 1;
+        // C를 빠르게 처리하기 위해서는 연결리스트가 Good
+        // Z를 빠르게 처리하기 위해서는 삭제 내역을 관리 Stack + 연결리스트가 Good
+        // 연결 리스트를 쓰더라도 U/D에서 모든 이동 거리의 합이 1_000_000이하인 경우만 주어지므로 OK
+        Node pre = new Node(null, null, 0);
+        Node cursor = pre;
+        for(int i = 1; i < n; ++i) {
+            Node node = new Node(pre, null, i);
+            pre.next = node;
+            pre = node;
+            
+            if(i == k) cursor = node;
         }
         
-        next[n-1] = -1;
-        
-        for(String command : cmd) {
+        Deque<Node> stack = new ArrayDeque<>();
+        for(String command : cmd) { // O(200_000)
             String[] info = command.split(" ");
+            String type = info[0];
             
-            if(info[0].equals("U")) {
-                int size = Integer.valueOf(info[1]);
-                while(size > 0) {
-                    cursor = prev[cursor];
-                    size--;
-                }
-                continue;
+            if(type.equals("U")) {
+                int x = Integer.parseInt(info[1]);
+                for(int i = 0; i < x; ++i) cursor = cursor.pre;
             }
             
-            if(info[0].equals("D")) {
-                int size = Integer.valueOf(info[1]);
-                while(size > 0) {
-                    cursor = next[cursor];
-                    size--;
-                }
-                continue;
+            if(type.equals("D")) {
+                int x = Integer.parseInt(info[1]);
+                for(int i = 0; i < x; ++i) cursor = cursor.next;
             }
             
-            if(info[0].equals("C")) {
-                history.push(cursor);
-                if(prev[cursor] != -1) next[prev[cursor]] = next[cursor];
-                if(next[cursor] != -1) prev[next[cursor]] = prev[cursor];
-                if(next[cursor] != -1) cursor = next[cursor];
-                if(cursor == history.peek()) cursor = prev[cursor];
-                continue;
+            if(type.equals("C")) {
+                stack.push(cursor);
+                if(cursor.pre != null) cursor.pre.next = cursor.next;
+                if(cursor.next != null) cursor.next.pre = cursor.pre;
+                cursor = (cursor.next) == null ? cursor.pre : cursor.next;
             }
             
-            if(info[0].equals("Z")) {
-                Integer toZ = history.pop();
-                if(prev[toZ] != -1) next[prev[toZ]] = toZ;
-                if(next[toZ] != -1) prev[next[toZ]] = toZ;
-                continue;
+            if(type.equals("Z")) {
+                Node poped = stack.pop();
+                if(poped.pre != null) poped.pre.next = poped;
+                if(poped.next != null) poped.next.pre = poped;
             }
         }
         
-        StringBuilder sb = new StringBuilder("O".repeat(n));
-        for(int deleted : history) sb.setCharAt(deleted, 'X');
+        boolean[] delete = new boolean[n];
+        while(!stack.isEmpty()) {
+            delete[stack.pop().num] = true;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; ++i) {
+            sb.append(delete[i] ? "X" : "O");
+        }
         
         return sb.toString();
+    }
+    
+    public class Node {
+        Node pre, next;
+        int num;
+        
+        public Node(Node pre, Node next, int num) {
+            this.pre = pre;
+            this.next = next;
+            this.num = num;
+        }
     }
 }

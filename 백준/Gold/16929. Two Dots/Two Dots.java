ce6a5d0818bcb6252852c9f66b5bfc94c4bede13
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-03-04 20:32:48 +0900

    [Gold IV] Title: Two Dots, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-// 21:37 시작!
 public class Main
 {
     /**
-     * 
+     * 게임 보드에서 사이클을 찾기
+     * 1. 모든 칸을 기준으로 DFS를 수행, 만약 DFS 과정에서 이미 방문한 점을 또 간다면?
+     *    사이클이 존재하는 것! but 바로 뒤로 돌아갈수도 있으니 어떤 점으로부터 왔는지 넘겨주기
      */
     static int n, m;
     static char[][] board;
-    static int[][] visit;
+    static boolean[][] visit;
     static boolean cycle;
 	public static void main(String[] args) throws Exception {
 	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -17,20 +18,21 @@ public class Main
 	    n = Integer.parseInt(st.nextToken());
 	    m = Integer.parseInt(st.nextToken());
 	    board = new char[n][m];
-	    
 	    for(int i = 0; i < n; ++i) {
 	        board[i] = bf.readLine().toCharArray();
 	    }
-
+	    
+	    visit = new boolean[n][m];
 	    for(int i = 0; i < n; ++i) {
 	        for(int j = 0; j < m; ++j) {
-	            visit = new int[n][m];
-	            visit[i][j] = 1;
-	            dfs(i, j, i, j);
+	            if(visit[i][j]) continue;
+
+	            visit[i][j] = true;
+	            dfs(i, j, -1, -1);
 	            
 	            if(cycle) {
 	                System.out.println("Yes");
-	                return;
+	                return;   
 	            }
 	        }
 	    }
@@ -40,21 +42,19 @@ public class Main
 	
 	static int[] dx = { -1, 0, 1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
-	public static void dfs(int x, int y, int sx, int sy) {
+	public static void dfs(int x, int y, int preX, int preY) {
 	    for(int d = 0; d < 4; ++d) {
 	        int nx = x + dx[d];
 	        int ny = y + dy[d];
 	        
 	        if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == board[x][y]) {
-	            if(nx == sx && ny == sy && visit[x][y] - visit[sx][sy] >= 3) {
-	                cycle = true;
-	                return;
+	            if(visit[nx][ny]) {
+	                if(nx != preX || ny != preY) cycle = true;
+	                continue;
 	            }
 	            
-	            if(visit[nx][ny] > 0) continue;
-	            
-	            visit[nx][ny] = visit[x][y] + 1;
-	            dfs(nx, ny, sx, sy);
+	            visit[nx][ny] = true;
+	            dfs(nx, ny, x, y);
 	        }
 	    }
 	}

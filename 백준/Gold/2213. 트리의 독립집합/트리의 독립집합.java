2097db80d5a04dad690cac9bab9af3cec3d25a67
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-03-11 17:07:32 +0900

    [Gold I] Title: 트리의 독립집합, Time: 260 ms, Memory: 24796 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.java
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.java
@@ -1,86 +1,119 @@
 import java.io.*;
 import java.util.*;
 
-// 16:23 시작!
+// 트리의 독립 집합 (15:37 ~ )
 public class Main
 {
-    static int[] w;
-    static int[][] dp;
+    /**
+     * 트리의 최대 독립 집합을 구하기 
+     * 
+     * 1(10) - 2(30) - 3(40) - 4(10) -5(20)
+     *         |
+     *         6(20) - 7(70)
+     * 
+     * 1. 먼저 독립 집합을 구하자.
+     *  1) 아무 노드나 선택 (1)
+     *  2) 1번 노드를 선택 or 1번 노드를 선택 X
+     *   2-1) 1번 노드를 선택, 1번과 인접한 노드는 선택 X
+     *   2-2) 1번 노드를 선택 X, 1번과 인접한 노드는 상관 X
+     *  3) 1번 노드와 인접한 노드를 탐색 (1번 노드 선택 여부 또는 해당 노드는 상태를 넘겨주기)
+     *   3-1) 자신이 선택 X이면, 자신과 인접한 노드는 상관 X
+     *   3-2) 자신이 선택 O이면, 자신과 인접한 노드는 선택 X
+     *  4) 2-3을 반복
+     * 
+     * 2. 독립 집합의 크기를 계산하고, 최댓값을 취하자!
+     * 
+     * 즉, 재귀
+     */
+    static int n;
+    static int[] value;
     static List<Integer>[] tree;
+    static int[][] dp;
 	public static void main(String[] args) throws Exception {
 	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	    int n = Integer.parseInt(bf.readLine());
-	    w = new int[n + 1];
+	    n = Integer.parseInt(bf.readLine());
+	    value = new int[n+1];
 	    
 	    StringTokenizer st = new StringTokenizer(bf.readLine());
 	    for(int i = 1; i <= n; ++i) {
-	        w[i] = Integer.parseInt(st.nextToken());
+	        value[i] = Integer.parseInt(st.nextToken());
 	    }
 	    
-	    tree = new List[n + 1];
-	    for(int i = 0; i <= n; ++i) {
-	        tree[i] = new ArrayList<>();
-	    }
+	    tree = new List[n+1];
+	    for(int i = 0; i <= n; ++i) tree[i] = new ArrayList<>();
 	    
-	    for(int i = 0; i < n - 1; ++i) {
+	    for(int i = 0; i < n-1; ++i) {
 	        st = new StringTokenizer(bf.readLine());
-	        int from = Integer.parseInt(st.nextToken());
-	        int to = Integer.parseInt(st.nextToken());
+	        int u = Integer.parseInt(st.nextToken());
+	        int v = Integer.parseInt(st.nextToken());
 	        
-	        tree[from].add(to);
-	        tree[to].add(from);
+	        tree[u].add(v);
+	        tree[v].add(u);
 	    }
 	    
-	    dp = new int[n + 1][2];
-	    find(1, 0);
+	    dp = new int[n+1][2];
 
-        List<Integer> nodes = new ArrayList<>();
-        if(dp[1][0] >= dp[1][1]) {
-            System.out.println(dp[1][0]);
-            trace(1, 0, 0, nodes);
-        } else {
-            System.out.println(dp[1][1]);
-            trace(1, 0, 1, nodes);
+	    int result1 = findMax(1, 0, true);
+	    int result2 = findMax(1, 0, false);
+	    
+	    StringBuilder sb = new StringBuilder();
+	    sb.append(Math.max(result1, result2)).append("\n");
+	    
+	    List<Integer> list = new ArrayList<>();
+
+	    if(result1 > result2) trace(1, 0, true, list);
+	    else trace(1, 0, false, list);
+	    
+	    list.sort((a, b) -> a - b);
+        for(int node : list) {
+            sb.append(node).append(" ");
         }
         
-        nodes.sort((a, b) -> a - b);
-        for(int node : nodes) {
-            System.out.print(node + " ");
-        }
+        System.out.println(sb.toString());
 	}
 	
-	public static void find(int node, int parent) {
-	    for(int child : tree[node]) {
-	        if(child == parent) continue;
+	// node를 포함한 하위 트리의 최대 독립 집합
+	public static int findMax(int node, int parent, boolean selected) {
+	    int sum = 0;
+	    if(selected) {
+	        if(dp[node][1] > 0) return dp[node][1];
+	        for(int child : tree[node]) {
+	            if(child == parent) continue;
+	            sum += findMax(child, node, false);
+	        }
 	        
-	        find(child, node);
-
-            dp[node][0] += Math.max(dp[child][0], dp[child][1]);
-            dp[node][1] += dp[child][0];
+	        return dp[node][1] = sum + value[node];
 	    }
 	    
-	    dp[node][1] += w[node];
+	    // node를 선택 X
+	    if(dp[node][0] > 0) return dp[node][0];
+	    for(int child : tree[node]) {
+            if(child == parent) continue;
+            sum += Math.max(findMax(child, node, true), findMax(child, node, false));
+        }
+        
+        return dp[node][0] = sum;
 	}
 	
-	public static void trace(int node, int parent, int state, List<Integer> path) {
-	    if(state == 1) {
-			path.add(node);
-			
-			for(int child : tree[node]) {
-			    if(child == parent) continue;
-				trace(child, node, 0, path);
-			}
-			
-			return;
-		}
-		
-		for(int child : tree[node]) {
-		    if(child == parent) continue;
-			if(dp[child][1] > dp[child][0]) {
-				trace(child, node, 1, path);
-			} else {
-				trace(child, node, 0, path);
-			}
-		}
+	public static void trace(int node, int parent, boolean selected, List<Integer> list) {
+	    if(selected) {
+	        list.add(node);
+	        
+	        for(int child : tree[node]) {
+	            if(child == parent) continue;
+	            trace(child, node, false, list);
+	        }
+	        
+	        return;
+	    }
+
+	    for(int child : tree[node]) {
+            if(child == parent) continue;
+            if(dp[child][0] > dp[child][1]) {
+                trace(child, node, false, list);
+            } else {
+                trace(child, node, true, list);
+            }
+	    }
 	}
 }

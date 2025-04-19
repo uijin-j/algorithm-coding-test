c3af4a474ed796f75fdd1d0839421441db22eebc
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-04-19 23:29:40 +0900

    [level 3] Title: 여행경로, Time: 1.17 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,55 +1,54 @@
 import java.util.*;
-import java.util.Collectors.*;
 
 class Solution {
-    // dfs로 풀면 되겠다!
-    Map<String, List<String>> map;
-    List<String> route;
-    String[] answer;
-    boolean flag;
+    int n;
+    Map<String, List<Ticket>> map;
+    boolean[] check;
+    String[] path;
+    boolean found;
     public String[] solution(String[][] tickets) {
-        int n = tickets.length;
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         map = new HashMap<>();
-        
-        for(String[] ticket : tickets) {
+        n = tickets.length;
+        for(int i = 0; i < n; ++i) {
+            String[] ticket = tickets[i];
             map.putIfAbsent(ticket[0], new ArrayList<>());
-            map.get(ticket[0]).add(ticket[1]);
+            map.get(ticket[0]).add(new Ticket(ticket[1], i));
         }
         
-        for(String key : map.keySet()) {
-            map.put(key, 
-                    map.get(key)
-                        .stream()
-                        .sorted()
-                        .collect(Collectors.toList())
-                   );
-        }
+        check = new boolean[n];
+        path = new String[n+1];
+        found = false;
+        dfs(0, "ICN");
         
-        route = new ArrayList<>();
-        route.add("ICN");
-        
-        dfs(0, n);
-        
-        return answer;
+        return path;
     }
     
-    public void dfs(int level, int goal) {
-        if(flag) return;
-        if(level == goal) {
-            answer = new String[route.size()];
-            int idx = 0;
-            for(String r : route) {
-                answer[idx++] = r;
-            }
-
-            flag = true;
+    public void dfs(int level, String from) {
+        if(found) return;
+        
+        path[level] = from;
+        
+        if(level == n) {
+            found = true;
             return;
         }
         
-        for(String to : map.get(route.get(level))) {
-            route.add(to);
-            dfs(level + 1, goal);
-            route.remove(level + 1);
+        for(Ticket next : map.getOrDefault(from, new ArrayList<>())) {
+            if(check[next.id]) continue;
+            check[next.id] = true;
+            dfs(level + 1, next.to);
+            check[next.id] = false;
+        }
+    }
+    
+    public class Ticket {
+        int id;
+        String to;
+        
+        public Ticket(String to, int id) {
+            this.to = to;
+            this.id = id;
         }
     }
 }

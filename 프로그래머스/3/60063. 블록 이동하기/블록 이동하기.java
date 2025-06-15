11fbde7a29952cbb05eda6075c9e0758112d04c1
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-06-15 23:09:08 +0900

    [level 3] Title: 블록 이동하기, Time: 169.89 ms, Memory: 272 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -7,65 +7,59 @@ class Solution {
         Set<String> visited = new HashSet<>();
         visited.add("0_0_0");
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 0});
-        int steps = 0;
-       
+        q.add(new int[]{0, 0, 0, 0});
         while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] cur = q.poll();
-                int y = cur[0], x = cur[1], o = cur[2];
-               
-                if ((y == n - 1 && x == n - 2 && o == 0)
-                    || (y == n - 2 && x == n - 1 && o == 1)) {
-                    return steps;
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], o = cur[2], t = cur[3];
+
+            if ((x == n - 1 && y == n - 2 && o == 0)
+                || (x == n - 2 && y == n - 1 && o == 1)) {
+                return t;
+            }
+
+            if (o == 0) { // 가로
+                if (y < n - 2 && board[x][y + 2] == 0) {
+                    handle(x, y + 1, 0, t + 1, q, visited); // 오른쪽
                 }
-               
-                if (o == 0) {
-                    if (x < n - 2 && board[y][x + 2] == 0) {
-                        handle(y, x + 1, 0, q, visited);
-                    }
-                    if (x > 0 && board[y][x - 1] == 0) {
-                        handle(y, x - 1, 0, q, visited);
-                    }
-                    if (y < n - 1 && board[y + 1][x] == 0 && board[y + 1][x + 1] == 0) {
-                        handle(y + 1, x, 0, q, visited);
-                        handle(y, x, 1, q, visited);
-                        handle(y, x + 1, 1, q, visited);
-                    }
-                    if (y > 0 && board[y - 1][x] == 0 && board[y - 1][x + 1] == 0) {
-                        handle(y - 1, x, 0, q, visited);
-                        handle(y - 1, x, 1, q, visited);
-                        handle(y - 1, x + 1, 1, q, visited);
-                    }
-                } else {
-                    if (x < n - 1 && board[y][x + 1] == 0 && board[y + 1][x + 1] == 0) {
-                        handle(y, x + 1, 1, q, visited);
-                        handle(y, x, 0, q, visited);
-                        handle(y + 1, x, 0, q, visited);
-                    }
-                    if (x > 0 && board[y][x - 1] == 0 && board[y + 1][x - 1] == 0) {
-                        handle(y, x - 1, 1, q, visited);
-                        handle(y, x - 1, 0, q, visited);
-                        handle(y + 1, x - 1, 0, q, visited);
-                    }
-                    if (y < n - 2 && board[y + 2][x] == 0) {
-                        handle(y + 1, x, 1, q, visited);
-                    }
-                    if (y > 0 && board[y - 1][x] == 0) {
-                        handle(y - 1, x, 1, q, visited);
-                    }
+                if (y > 0 && board[x][y - 1] == 0) {
+                    handle(x, y - 1, 0, t + 1, q, visited); // 왼쪽
+                }
+                if (x < n - 1 && board[x + 1][y] == 0 && board[x + 1][y + 1] == 0) {
+                    handle(x + 1, y, 0, t + 1, q, visited); // 아래
+                    handle(x, y, 1, t + 1, q, visited);
+                    handle(x, y + 1, 1, t + 1, q, visited);
+                }
+                if (x > 0 && board[x - 1][y] == 0 && board[x - 1][y + 1] == 0) {
+                    handle(x - 1, y, 0, t + 1, q, visited); // 위
+                    handle(x - 1, y, 1, t + 1, q, visited);
+                    handle(x - 1, y + 1, 1, t + 1, q, visited);
+                }
+            } else { // 세로
+                if (y < n - 1 && board[x][y + 1] == 0 && board[x + 1][y + 1] == 0) {
+                    handle(x, y + 1, 1, t + 1, q, visited); // 오른쪽
+                    handle(x, y, 0, t + 1, q, visited);
+                    handle(x + 1, y, 0, t + 1, q, visited);
+                }
+                if (y > 0 && board[x][y - 1] == 0 && board[x + 1][y - 1] == 0) {
+                    handle(x, y - 1, 1, t + 1, q, visited); // 왼쪽
+                    handle(x, y - 1, 0,  t + 1, q, visited); 
+                    handle(x + 1, y - 1, 0, t + 1, q, visited);
+                }
+                if (x < n - 2 && board[x + 2][y] == 0) {
+                    handle(x + 1, y, 1, t + 1, q, visited); // 아래
+                }
+                if (x > 0 && board[x - 1][y] == 0) {
+                    handle(x - 1, y, 1, t + 1, q, visited); // 위
                 }
             }
-            steps++;
         }
        
         return -1;
     }
    
-    private void handle(int y, int x, int orientation, Queue<int[]> q, Set<String> visited) {
-        if (visited.add(String.format("%d_%d_%d", y, x, orientation))) {
-            q.add(new int[]{y, x, orientation});
+    private void handle(int x, int y, int orientation, int time, Queue<int[]> q, Set<String> visited) {
+        if (visited.add(String.format("%d_%d_%d", x, y, orientation))) {
+            q.add(new int[]{x, y, orientation, time});
         }
     }
 }

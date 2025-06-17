9f17131f6e11446b8ed9cbb774aff16896889266
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-06-18 00:35:07 +0900

    [level 4] Title: [3차] 자동완성, Time: 250.83 ms, Memory: 359 MB -BaekjoonHub

--- 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -1,44 +1,6 @@
-import java.util.*;
-
-// 00:19 시작
 class Solution {
-    /**
-     * prefix로 단어를 찾기? => 트라이
-     */
-    public class Trie {
-        Map<Character, Trie> children;
-        int numOfChildren;
-        
-        public Trie() {
-            children = new HashMap<>();
-            numOfChildren = 0;
-        }
-    }
-    
     public int solution(String[] words) {
-        Trie root = new Trie();
-        for(String word : words) {
-            Trie node = root;
-            node.numOfChildren++;
-            for(int i = 0; i < word.length(); ++i) {
-                char ch = word.charAt(i);
-                node.children.putIfAbsent(ch, new Trie());
-                node = node.children.get(ch);
-                node.numOfChildren++;
-            }
-        }
-        
         int answer = 0;
-        for(String word : words) {
-            Trie node = root;
-            for(int i = 0; i < word.length(); ++i) {
-                answer++;
-                char ch = word.charAt(i);
-                node = node.children.get(ch);
-                if(node.numOfChildren == 1) break;
-            }
-        }
-        
         return answer;
     }
 }

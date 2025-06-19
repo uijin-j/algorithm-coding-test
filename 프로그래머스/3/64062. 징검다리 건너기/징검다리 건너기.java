07b5cb43f284020e886442c22db5c01bbd14224b
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-06-19 23:50:48 +0900

    [level 3] Title: 징검다리 건너기, Time: 86.99 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,39 +1,6 @@
-import java.util.*;
-
-// 23:31 시작!
 class Solution {
-    /**
-     * 완탐? O(200_000 * 200_000_000) 시간초과
-     * K 크기의 구간들 중 구간의 최댓값이 최소인 구간을 찾기!
-     * 투 포인터? 
-     */
     public int solution(int[] stones, int k) {
-        int n = stones.length;
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> b.num - a.num);
-        for(int i = 0; i < k; ++i) {
-            pq.offer(new Node(i, stones[i]));
-        }
-        
-        int min = pq.peek().num;
-        for(int to = k, from = 1; to < n; ++to, ++from) {
-            pq.offer(new Node(to, stones[to]));
-            while(pq.peek().idx < from) {
-                pq.poll();
-            }
-            
-            min = Math.min(min, pq.peek().num);
-        }
-        
-        return min;
-    }
-    
-    public class Node {
-        int idx, num;
-        
-        public Node(int idx, int num) {
-            this.idx = idx;
-            this.num = num;
-        }
+        int answer = 0;
+        return answer;
     }
 }

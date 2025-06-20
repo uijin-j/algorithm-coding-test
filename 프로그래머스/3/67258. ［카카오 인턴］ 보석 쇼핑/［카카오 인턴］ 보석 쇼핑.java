81dcdc7d1976b497755f140e4a9a9c9665556e5c
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-06-20 22:29:30 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 49.55 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,31 +1,53 @@
 import java.util.*;
 
+// 21:48 시작!
 class Solution {
+    /**
+     * 1) 이분탐색? k 구간으로 쇼핑을 할 수 있는가? O(nlogn)
+     * 2) 투 포인터? 조건을 만족하는 구간을 찾은 후 구간을 줄이기
+     */
     public int[] solution(String[] gems) {
-        Set<String> types = new HashSet<>();
-        for(String gem : gems) types.add(gem);
+        Set<String> set = new HashSet<>();
+        for(String gem : gems) set.add(gem);
+        int numOfGem = set.size();
         
-        int total = types.size(); // total 종류의 보석이 있음
-        Map<String, Integer> count = new HashMap<>();
-        int length = gems.length;
-        int[] answer = new int[]{1, length};
         int left = 0, right = 0;
-        while(right < gems.length) {
-            count.put(gems[right], count.getOrDefault(gems[right], 0) + 1);
-            
-            while(count.get(gems[left]) > 1) { // ❗️ 얘를 while로 해야함! 새로운 left도 확인해야 해서!
-                count.put(gems[left], count.get(gems[left]) - 1);
-                left++;
+        Map<String, Integer> map = new HashMap<>();
+        int type = 0;
+        while(type < numOfGem) {
+            String gem = gems[right++];
+            if(map.getOrDefault(gem, 0) == 0) {
+                type += 1;
             }
             
-            if(count.size() == total && length > (right - left + 1)) { // ❗️ 조건에 만족하고, 길이가 더 짧으면 선택!
-                length = right - left + 1;
-                answer = new int[]{left + 1, right + 1};
+            map.put(gem, map.getOrDefault(gem, 0) + 1);
+        }
+              
+        int n = gems.length;
+        int[] answer = new int[]{left, right - 1};
+        int len = right - left;
+        while(right <= n) {
+            String toRemove = gems[left++];
+            map.put(toRemove, map.get(toRemove) - 1);
+
+            if(map.get(toRemove) == 0) {
+                while(right < n && !gems[right].equals(toRemove)) {
+                    String toAdd = gems[right++];
+                    map.put(toAdd, map.get(toAdd) + 1);
+                }
+
+                if(right == n) break;
+                map.put(gems[right], 1);
+                right++;
             }
-            
-            right++;
+
+            if(len > right - left) {
+                answer = new int[]{left, right - 1};
+                len = right - left;
+            }
         }
         
+        for(int i = 0; i < 2; ++i) answer[i] +=1;
         return answer;
     }
 }

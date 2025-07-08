f135552972c55a12578262766e93410ef27a21a0
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-07-08 23:15:06 +0900

    [level 4] Title: 튜브의 소개팅, Time: 132.82 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/4/1839. 튜브의 소개팅/튜브의 소개팅.java
+++ 프로그래머스/4/1839. 튜브의 소개팅/튜브의 소개팅.java
@@ -1,65 +1,61 @@
 import java.util.*;
 
 class Solution {
-    /** 
-     * (0, 0)에서 (m-1, n-1)까지 가는 최단경로
-     * but 제약이 존재 (경로 상의 t의 합이 s를 넘지 않아야 함)
+    /**
+     * (0,0) → (m-1, n-1)로 가는 최단거리 ==> 완탐? O(nm)
+     * 그런데.. 지나온 길의 t의 합이 s를 넘지 않아야 함 ==> 기존에 왔던 sum(t')가 현재 sum(t)보다 크면, 재방문 가능!
      */
     int[] dx = { -1, 0, 1, 0 };
     int[] dy = { 0, 1, 0, -1 };
     public int[] solution(int m, int n, int s, int[][] time_map) {
+        long[][] visit = new long[m][n];
+        for(int i = 0; i < m; ++i) Arrays.fill(visit[i], Long.MAX_VALUE);
+        
         Queue<Node> q = new LinkedList<>();
+        visit[0][0] = 0;
         q.offer(new Node(0, 0, 0, 0));
-        int minDist = Integer.MAX_VALUE;
-        int minTime = Integer.MAX_VALUE;
-        long[][][] visit = new long[m][n][2];
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                Arrays.fill(visit[i][j], Long.MAX_VALUE);
-            }
-        }
+        
+        int len = Integer.MAX_VALUE;
+        int time = Integer.MAX_VALUE;
         while(!q.isEmpty()) {
-            Node node = q.poll();
+            Node cur = q.poll();
             
-            if(visit[node.x][node.y][0] <= node.dist && visit[node.x][node.y][1] <= node.time) {
-                continue;
-            }
-            visit[node.x][node.y][0] = Math.min(visit[node.x][node.y][0], node.dist);
-            visit[node.x][node.y][1] = Math.min(visit[node.x][node.y][1], node.time);
-            
-            
-            if(node.x == m-1 && node.y == n-1) {
-                if(minDist > node.dist) {
-                    minDist = node.dist;
-                    minTime = (int) node.time;
+            if(cur.x == m-1 && cur.y == n-1) {
+                if(len > cur.cnt) {
+                    len = cur.cnt;
+                    time = (int) cur.time;
                 }
                 
-                if(minDist == node.dist && minTime > node.time) minTime = (int) node.time;
+                if(len == cur.cnt && time > cur.time) {
+                    time = (int) cur.time;
+                }
             }
             
             for(int d = 0; d < 4; ++d) {
-                int nx = node.x + dx[d];
-                int ny = node.y + dy[d];
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
                 
                 if(nx >= 0 && nx < m && ny >= 0 && ny < n && time_map[nx][ny] != -1) {
-                    long nextTime = node.time + time_map[nx][ny];
-                    if(nextTime > s) continue;
-                    q.offer(new Node(nx, ny, node.dist + 1, nextTime));
+                    long nextTime = cur.time + (long) time_map[nx][ny];
+                    if(nextTime <= s && nextTime < visit[nx][ny]) {
+                        visit[nx][ny] = nextTime;
+                        q.offer(new Node(nx, ny, cur.cnt + 1, nextTime));
+                    }
                 }
             }
         }
         
-        return new int[]{minDist, minTime};
+        return new int[]{len, time};
     }
     
     public class Node {
-        int x, y, dist;
+        int x, y, cnt;
         long time;
         
-        public Node(int x, int y, int dist, long time) {
+        public Node(int x, int y, int cnt, long time) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+            this.cnt = cnt;
             this.time = time;
         }
     }

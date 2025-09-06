a5f3e4e36ef2ee0467a8a32ca6849c56e3795b31
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-09-06 22:47:34 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1175.48 ms, Memory: 404 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,59 +1,57 @@
 import java.util.*;
 
+/**
+ * 22:24 시작!
+ * (x, y)에서 (r, c)로 k 거리로 가기 (중복 방문 OK)
+ * 경로가 사전 순으로 가장 빨라야 함 (d > l > r > u)
+ * 탈출이 불가능한 경우 impossible
+ * 
+ * 완탐을 해야할 것 같음 --> 중복 방문이 가능하지만 거리가 k를 넘어서면 탐색이 종료되기 때문에 시간 초과가 안 날수도?
+ */
 class Solution {
-    /**
-     * (x, y) -> (r, c)로 가는데 중복 방문 ⭕️, 거리 고정 ⭕️
-     * 최단거리를 구하는 dfs 방식으로는 시간초과 O(4^k)
-     * => 계산을 줄일 수 있는 방법을 찾아보기!
-     */
-    int n, m;
-    int[] target;
-    boolean found;
-    String answer = "impossible";
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        this.n = n;
-        this.m = m;
-        target = new int[]{ r - 1, c - 1 };
-        
-        int min = Math.abs(x - r) + Math.abs(y - c); // 최단거리
-        if( k < min || (k - min) % 2 == 1) {
-            return "impossible"; // 도달할 수 없는 경우
-        }
-        
-        dfs(0, x - 1, y - 1, k, new StringBuilder());
-        
-        return answer;
-    }
-    
-    String[] d = { "d", "l", "r", "u" };
     int[] dx = { 1, 0, 0, -1 };
     int[] dy = { 0, -1, 1, 0 };
-    public void dfs(int level, int x, int y, int goal, StringBuilder sb) {
-        if(found) {
-            return;
+    String[] direction = { "d", "l", "r", "u" };
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        boolean[][][] check = new boolean[n+1][m+1][k+1];
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y, 0, ""));
+        check[x][y][0] = true;
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            
+            for(int d = 0; d < 4; ++d) {
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
+                int nd = cur.dist + 1;
+                
+                if(nx > 0 && nx <= n && ny > 0 && ny <= m && !check[nx][ny][nd]) {
+                    if(nd == k && nx == r && ny == c) {
+                        return cur.path + direction[d];
+                    }
+                    
+                    // 남은 거리로 절대 정답이 불가능하면 넘어가기
+                    int dist = Math.abs(r - nx) + Math.abs(c - ny);
+                    if(dist > k - nd) continue;
+                    
+                    check[nx][ny][nd] = true;
+                    q.offer(new Node(nx, ny, nd, cur.path + direction[d]));
+                }
+            }
         }
         
-        if(level == goal) {
-            if(x == target[0] && y == target[1]) {
-                found = true;
-                answer = sb.toString();
-            }
-            
-            return;
-        }
+        return "impossible";
+    }
+    
+    public class Node {
+        int x, y, dist;
+        String path;
         
-        int min = Math.abs(x - target[0]) + Math.abs(y - target[1]);
-        if(goal - level < min || (goal - level - min) % 2 == 1) return; // 도달할 수 없는 경우
-        
-        for(int i = 0; i < 4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                sb.append(d[i]);
-                dfs(level + 1, nx, ny, goal, sb);
-                sb.deleteCharAt(level);
-            }
+        public Node(int x, int y, int dist, String path) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+            this.path = path;
         }
     }
 }

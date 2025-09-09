24f13a55f79a79dee9b064a4fae3a7159820a1bc
uijin-j <80446430+uijin-j@users.noreply.github.com>
2025-09-09 23:33:18 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.08 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,62 +1,62 @@
+import java.util.*;
+
+/**
+ * 23:00 시작!
+ * 합이 n+1인 카드 2장을 내고 다음 라운드로 넘어가는 게임을 진행
+ * n/3장의 카드를 가지고 시작
+ * 매 라운드마다 2장의 카드를 확인 후, 코인을 주고 가지거나 버림
+ *
+ * 도달 가능한 최대 라운드?
+ * 
+ * 일단 지금 보유하고 있는 카드랑 짝이면 가지는 것이 이득
+ * 만약, 지금 보유하고 있는 카드랑 짝이 아니면? 미래에 나오는 카드와 짝이 될 수 있으니 마킹
+ * 만약, 마킹된 카드랑 짝이면? 코인 2개 주고 살 수 있는 카드가 있다고 체크
+ * 그리디하게 최대한 코인을 적게 쓰면서 라운드를 진행!
+ */
 class Solution {
-    /**
-     * 모든 카드는 n+1을 만들 수 있는 짝이 딱 1장 있음!
-     *  - 첫번째 목표는 현재 라운드를 통과하기 (합이 n+1인 쌍을 만들기)
-     *  - 두번째 목표는 앞으로 라운드도 통과할 수 있도록 하기 (합이 n+1인 쌍을 최대한 많이 확보 + 최대한 빨리 확보)
-     */
     public int solution(int coin, int[] cards) {
-        int n = cards.length; // n은 최대 1000
-        int target = n + 1;
-        boolean[] inHand = new boolean[n+1]; // 내가 손에 쥐고 있는 카드
-        boolean[] canBuy = new boolean[n+1]; // 내가 코인 주고 살 수 있는 카드
+        int n = cards.length;
+        boolean[] mine = new boolean[n+1];
+        boolean[] seen = new boolean[n+1];
         int pair = 0;
-        for(int i = 0; i < n / 3; ++i) {
-            if(inHand[target - cards[i]]) {
-                pair++;
-                continue; // 무조건 한 쌍이니까, 굳이 hand에 체크 해 놓을 필요가 없음!
-            }
-            
-            inHand[cards[i]] = true;
+        int spare = 0;
+        
+        for(int i = 0; i < n/3; ++i) {
+            int card = cards[i];
+            if(mine[n+1-card]) pair++;
+            mine[card] = true;
         }
         
-        int round = 1;
-        int idx = n / 3;
-        int pairInCanBuy = 0; // canBuy 배열에 있는 카드 중 페어의 갯수
-        while(idx < n) {
-            for(int i = 0; i < 2; ++i) {
-                if(coin == 0) break; // 코인이 없으면 무조건 버려야 함
-                
-                int card = cards[idx++];
-                if(inHand[target - card]) { // 손에 있는 애랑 짝이면 무조건 사기 (코인 1개만 써도 되기 때문)
+        int answer = 1;
+        for(int i = n/3; i < n; i += 2, answer+=1) {    
+            for(int j = 0; j < 2; ++j) {
+                int card = cards[i+j];
+                if(mine[n+1-card] && coin > 0) {
                     coin--;
                     pair++;
-                    continue;
+                    mine[card] = true;
                 }
-
-                if(canBuy[target - card]) {
-                    pairInCanBuy++;
-                    continue;
+                
+                if(seen[n+1-card]) {
+                    spare++;
                 }
-
-                canBuy[card] = true; // 미래를 위해 살 수 있는 리스트에 추가
+                seen[card] = true;
             }
             
             if(pair > 0) {
                 pair--;
-                round++;
                 continue;
             }
             
-            if(coin >= 2 && pairInCanBuy > 0) {
-                coin -= 2;
-                pairInCanBuy -= 1;
-                round++;
+            if(spare > 0 && coin >= 2) {
+                spare--;
+                coin-=2;
                 continue;
             }
             
-            return round;
+            break;
         }
         
-        return round;
+        return answer;
     }
 }
